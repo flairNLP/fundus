843451a5674d17f9b1464d0813c5a8a32b4c4be5
MaxDall <max.dallabetta@googlemail.com>
2022-11-04 16:43:19 +0100

    extended Registered function to be usable as instance method as well as moving some stuff around.

--- src/parser/html_parser/base_parser.py
+++ src/parser/html_parser/base_parser.py
@@ -1,32 +1,35 @@
 import functools
 import inspect
 import json
-from typing import List, Callable, Dict, Optional, Any, Literal, Union
+from abc import ABC
+from typing import List, Callable, Dict, Optional, Any, Literal, Union, Set
 
 import lxml.html
 
-from src.html_parser.utility import get_meta_content
+from src.parser.html_parser.utility import get_meta_content
 
 
 class RegisteredFunction:
-    __wrapped__ = None
+    __wrapped__: callable = None
+    __func__: callable
+    __self__: object
 
     # TODO: ensure uint for priority instead of int
     def __init__(self,
-                 func: Union[Callable, functools.partial],
+                 func: Callable,
                  flow_type: str,
                  priority: Optional[int] = None):
 
-        self.func = func
+        self.__func__ = func
         self.flow_type = flow_type
         self.priority = priority
 
-    def attach(self, instance: object) -> 'RegisteredFunction':
-        self.func = functools.partial(self.func, self=instance)
+    def __get__(self, instance, owner):
+        self.__self__ = instance
         return self
 
     def __call__(self, *args, **kwargs):
-        return self.func(*args, *kwargs)
+        return self.__func__(self.__self__, *args, *kwargs)
 
     def __lt__(self, other):
         if self.priority is None:
@@ -68,13 +71,11 @@ def register_filter(cls=None, /, *, priority: int = None):
 
 class BaseParser:
 
-    def __init__(self, attr_error_handling: Literal['suppress', 'catch', 'raise'] = 'raise'):
-        self.attr_error_handling = attr_error_handling
+    def __init__(self):
         self._shared_object_buffer: Dict[str, Any] = {}
 
-        detached_functions = [func for _, func in
-                              inspect.getmembers(self, predicate=lambda x: isinstance(x, RegisteredFunction))]
-        registered_functions = [reg_func.attach(self) for reg_func in detached_functions]
+        registered_functions = [func for _, func in
+                                inspect.getmembers(self, predicate=lambda x: isinstance(x, RegisteredFunction))]
 
         self._func_flow: dict[str, RegisteredFunction] = {func.__name__: func for func in sorted(registered_functions)}
 
@@ -82,11 +83,11 @@ class BaseParser:
     def cache(self) -> Dict[str, Any]:
         return self._shared_object_buffer
 
-    @property
-    def attributes(self) -> List[str]:
-        return [func.__name__ for func in self._func_flow.values() if func.flow_type == 'attribute']
+    @classmethod
+    def attributes(cls):
+        return [func.__name__ for _, func in
+                inspect.getmembers(cls, predicate=lambda x: isinstance(x, RegisteredFunction))]
 
-    @register_function(priority=0)
     def _base_setup(self):
         content = self.cache['html']
         doc = lxml.html.fromstring(content)
@@ -95,13 +96,14 @@ class BaseParser:
         meta = get_meta_content(doc) or {}
         self.share(doc=doc, ld=ld, meta=meta)
 
-    def parse(self, html: str, **kwargs) -> Optional[Dict[str, Any]]:
+    def parse(self, html: str,
+              error_handling: Literal['suppress', 'catch', 'raise'] = 'raise') -> Optional[Dict[str, Any]]:
 
         # wipe existing shared cache
         self._wipe()
 
         # share html and kwargs among cache
-        self.share(html=html, **kwargs)
+        self.share(html=html)
 
         self._base_setup()
 
@@ -117,13 +119,13 @@ class BaseParser:
                 try:
                     article_cache[func.__name__] = func()
                 except Exception as err:
-                    if self.attr_error_handling == 'raise':
-                        raise err
-                    elif self.attr_error_handling == 'catch':
-                        article_cache[func.__name__] = err
-                    elif self.attr_error_handling == 'suppress':
-                        article_cache[func.__name__] = None
-
+                    match error_handling:
+                        case 'raise':
+                            raise err
+                        case 'catch':
+                            article_cache[func.__name__] = err
+                        case 'suppress':
+                            article_cache[func.__name__] = None
             elif func.flow_type == 'filter':
                 if func():
                     return None
