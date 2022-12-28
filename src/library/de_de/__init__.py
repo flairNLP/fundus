0c8b00b04401cd6c68ef368983c7ecc5ce0dd7cd
Aaron <aaronwey@posteo.de>
2022-12-28 14:21:02 +0100

    Implements FAZ-Crawler and utility related to parsing

--- src/library/de_de/__init__.py
+++ src/library/de_de/__init__.py
@@ -1,6 +1,7 @@
 from src.library.collection.base_objects import PublisherEnum, PublisherSpec
 from .die_welt_parser import DieWeltParser
 from .mdr_parser import MDRParser
+from .faz_parser import FAZParser
 
 
 # noinspection PyPep8Naming
@@ -9,3 +10,5 @@ class DE_DE(PublisherEnum):
                             parser=DieWeltParser)
     MDR = PublisherSpec(domain='https://www.mdr.de/', rss_feeds=['https://www.mdr.de/nachrichten/index-rss.xml'],
                         sitemaps=['https://www.mdr.de/news-sitemap.xml'], parser=MDRParser)
+    FAZ = PublisherSpec(domain='https://www.faz.net/', rss_feeds=['https://www.faz.net/rss/aktuell', 'https://www.faz.net/rss/aktuell/politik','https://www.faz.net/rss/aktuell/sport','https://www.faz.net/rss/aktuell/wirtschaft/','https://www.faz.net/rss/aktuell/gesellschaft/'],
+                        sitemaps=[], parser=FAZParser)
