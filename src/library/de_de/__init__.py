16976c08ca0550f2687df3b0628a3e655ccd4155
Aaron <aaronwey@posteo.de>
2023-01-02 12:14:08 +0100

    Adds the orf parser to the collection

--- src/library/de_de/__init__.py
+++ src/library/de_de/__init__.py
@@ -2,12 +2,11 @@ from src.library.collection.base_objects import PublisherEnum, PublisherSpec
 from .die_welt_parser import DieWeltParser
 from .mdr_parser import MDRParser
 from .faz_parser import FAZParser
+from .merkur_parser import MerkurParser
+from .orf_parser import OrfParser
 
 
 # noinspection PyPep8Naming
-from .merkur_parser import MerkurParser
-
-
 class DE_DE(PublisherEnum):
     DieWelt = PublisherSpec(domain='https://www.welt.de/', rss_feeds=['https://www.welt.de/feeds/latest.rss'],
                             parser=DieWeltParser)
@@ -16,3 +15,4 @@ class DE_DE(PublisherEnum):
     FAZ = PublisherSpec(domain='https://www.faz.net/', rss_feeds=['https://www.faz.net/rss/aktuell', 'https://www.faz.net/rss/aktuell/politik','https://www.faz.net/rss/aktuell/sport','https://www.faz.net/rss/aktuell/wirtschaft/','https://www.faz.net/rss/aktuell/gesellschaft/'],
                         sitemaps=[], parser=FAZParser)
     Merkur = PublisherSpec(domain='https://www.merkur.de/', rss_feeds=['https://www.merkur.de/welt/rssfeed.rdf'], sitemaps=[], parser=MerkurParser)
+    ORF = PublisherSpec(domain='https://www.orf.at', rss_feeds=["https://rss.orf.at/news.xml"], sitemaps=[], parser=OrfParser)
