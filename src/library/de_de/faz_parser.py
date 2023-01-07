bb3df395b1f71fe407397316c1593c4f35033e5b
Aaron <aaronwey@posteo.de>
2023-01-07 09:56:17 +0100

    Implements FAZ-Parser and utility related to parsing (#23)
    * Implements FAZ-Crawler and utility related to parsing
    
    * some formatting
    
    * Adressen @maxdalls comments and adds test coverage to generic_author_extraction
    
    * finish utility.py
    
    * fix plaintext extraction
    
    * Removes the tests from the pr and adds a type hint
    
    Co-authored-by: MaxDall <max.dallabetta@googlemail.com>

---
+++ src/library/de_de/faz_parser.py
@@ -0,0 +1,29 @@
+import datetime
+from typing import Optional, List
+
+from src.parser.html_parser import BaseParser, register_attribute
+from src.parser.html_parser.utility import generic_plaintext_extraction_with_css, generic_date_extraction, \
+    generic_author_extraction, generic_topic_extraction
+
+
+class FAZParser(BaseParser):
+
+    @register_attribute
+    def plaintext(self) -> Optional[str]:
+        return generic_plaintext_extraction_with_css(self.precomputed.doc, 'div.atc-Text > p')
+
+    @register_attribute
+    def topics(self) -> Optional[List[str]]:
+        return generic_topic_extraction(self.precomputed.meta)
+
+    @register_attribute
+    def publishing_date(self) -> Optional[datetime.datetime]:
+        return generic_date_extraction(self.precomputed.ld, "datePublished")
+
+    @register_attribute
+    def authors(self) -> List[str]:
+        return generic_author_extraction(self.precomputed.meta, ['author'])
+
+    @register_attribute
+    def title(self) -> Optional[str]:
+        return self.meta().get('og:title')
--- src/parser/html_parser/utility.py
+++ src/parser/html_parser/utility.py
@@ -1,7 +1,21 @@
 import re
-from typing import Dict, List, Optional
+from datetime import datetime
+from typing import Dict, List, Optional, Any
 
+import dateutil
 import lxml.html
+
+
+def _get_nested_value_with_key_path_as_list(source: Dict[Any, Any], key_list: List[str]) -> Any:
+    visited = []
+    cur = source
+    for key in key_list:
+        if not isinstance(cur, dict):
+            raise TypeError(f"Key path '{' -> '.join(visited)}' leads to an unsupported Value in between. Only dict "
+                            f"is allowed.")
+        cur = cur.get(key)
+        visited.append(key)
+    return cur
 
 
 def get_meta_content(tree: lxml.html.HtmlElement) -> Dict[str, str]:
@@ -15,3 +29,42 @@ def strip_nodes_to_text(text_nodes: List) -> Optional[str]:
     if not text_nodes:
         return None
     return "\n\n".join(([re.sub(r'\n+', ' ', node.text_content()) for node in text_nodes])).strip()
+
+
+def generic_author_extraction(source: Dict[str, any], key_list: List[str]) -> List[str]:
+    authors = _get_nested_value_with_key_path_as_list(source, key_list)
+
+    if not authors:
+        return []
+
+    if isinstance(authors, str):
+        authors = [authors]
+
+    elif isinstance(authors, list):
+        authors = [name for author in authors if (name := author.get('name'))]
+
+    elif isinstance(authors, dict):
+        authors = [name] if (name := authors.get('name')) else []
+
+    else:
+        raise TypeError(f"Value '{authors}' in 'source' dict with key path '{' -> '.join(key_list)}' has an unsupported"
+                        f"type. Supported types are 'str, list, dict'")
+
+    return authors
+
+
+def generic_plaintext_extraction_with_css(doc, selector: str) -> Optional[str]:
+    nodes = doc.cssselect(selector)
+    return strip_nodes_to_text(nodes)
+
+
+def generic_topic_extraction(meta: Dict[str, any], key_word: str = "keywords", delimiter: str = ',') -> List[str]:
+    if keyword_str := meta.get(key_word):
+        return [keyword.strip() for keyword in keyword_str.split(delimiter)]
+    return []
+
+
+def generic_date_extraction(meta, key_word: str = "datePublished") -> Optional[datetime]:
+    if date_str := meta.get(key_word):
+        return dateutil.parser.parse(date_str)
+    return None
