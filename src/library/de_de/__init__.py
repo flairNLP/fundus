76d154e59f28027710374acd5446dabafc5f9e87
Aaron <aaronwey@posteo.de>
2023-01-08 19:23:25 +0100

    Updates the focus parser to increase code quality

--- src/library/de_de/__init__.py
+++ src/library/de_de/__init__.py
@@ -13,14 +13,6 @@ class DE_DE(PublisherEnum):
                             parser=DieWeltParser)
     MDR = PublisherSpec(domain='https://www.mdr.de/', rss_feeds=['https://www.mdr.de/nachrichten/index-rss.xml'],
                         sitemaps=['https://www.mdr.de/news-sitemap.xml'], parser=MDRParser)
-    FAZ = PublisherSpec(domain='https://www.faz.net/', rss_feeds=['https://www.faz.net/rss/aktuell', 'https://www.faz.net/rss/aktuell/politik','https://www.faz.net/rss/aktuell/sport','https://www.faz.net/rss/aktuell/wirtschaft/','https://www.faz.net/rss/aktuell/gesellschaft/'],
-                        sitemaps=[], parser=FAZParser)
-    Focus = PublisherSpec(domain='https://www.focus.de/', rss_feeds=[ 'https://rss.focus.de/fol/XML/rss_folnews.xml'], sitemaps=[], parser=FocusParser)
-
-    MDR = PublisherSpec(domain='https://www.mdr.de/',
-                        rss_feeds=['https://www.mdr.de/nachrichten/index-rss.xml'],
-                        sitemaps=['https://www.mdr.de/news-sitemap.xml'],
-                        parser=MDRParser)
 
     FAZ = PublisherSpec(domain='https://www.faz.net/',
                         rss_feeds=['https://www.faz.net/rss/aktuell', 'https://www.faz.net/rss/aktuell/politik',
@@ -28,3 +20,6 @@ class DE_DE(PublisherEnum):
                                    'https://www.faz.net/rss/aktuell/wirtschaft/',
                                    'https://www.faz.net/rss/aktuell/gesellschaft/'],
                         parser=FAZParser)
+    Focus = PublisherSpec(domain='https://www.focus.de/', rss_feeds=[ 'https://rss.focus.de/fol/XML/rss_folnews.xml'], sitemaps=[], parser=FocusParser)
+
+
