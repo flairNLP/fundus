d18ff69877e6e0dd16d02d8a1ae149f0b1bbd5cc
MaxDall <46926170+MaxDall@users.noreply.github.com>
2023-01-18 10:19:00 +0100

    Add BF traversal for LinkedData (#40)
    * remove duplicate import
    
    * changed name of breadth_first_search to bf_search

--- src/parser/html_parser/base_parser.py
+++ src/parser/html_parser/base_parser.py
@@ -6,7 +6,7 @@ from abc import ABC
 from collections import defaultdict
 from copy import copy
 from dataclasses import dataclass, field
-from typing import Callable, Dict, Optional, Any, Literal, List, Union
+from typing import Callable, Dict, Optional, Any, Literal, List, Union, Iterable
 
 import lxml.html
 
@@ -100,7 +100,7 @@ class LinkedData:
 
         self._contains = [ld_type for ld_type in self._ld_by_type.keys() if ld_type is not None]
 
-    def get(self, key: str, default: any = None):
+    def get(self, key: str, default: Any = None):
         """
         This function acts like get() on pythons Mapping type with the difference that this method will
         iterate through all found ld types and return the first value where <key> matches. If no match occurs,
@@ -110,7 +110,7 @@ class LinkedData:
 
         :param key: The key to search vor
         :param default: The returned default if <key> is not found, default: None
-        :return:
+        :return: The reached value or <default>
         """
         for name, ld in sorted(self._ld_by_type.items(), key=lambda t: t[0]):
             if not name:
@@ -118,6 +118,77 @@ class LinkedData:
             elif value := ld.get(key):
                 return value
         return default
+
+    def get_value_by_key_path(self, key_path: List[str], default: Any = None):
+        """
+        Works like get() except this one assumes a path is given as list of keys (str).
+        I.e:
+            key_path := ["mainEntity", "author"], default := {}
+            results in self._ld_by_type.get("mainEntity").get("author")
+
+        Whenever a key is missing or an empty value occurs down the path this funktion will immediately return
+        <default>, but will not catch if not all values supports get()
+
+        :param key_path: A list of keys in order forming a path to the desired value
+        :param default: A default returned when either a key is missing or resulting in an empty/null value
+        :return: The reached value or <default>
+        """
+        tmp = self._ld_by_type.copy()
+        for key in key_path:
+            if not (tmp := tmp.get(key)):
+                return default
+        return tmp
+
+    def bf_search(self, key: str, depth: int = None) -> Any:
+        """
+        This is a classic BF search on the nested dicts representing the JSON-LD. <key> specifies the dict key to
+        search, <depth> the depth level. If the depth level is set to None, this method will search through the whole
+        LD. It is important to notice that this will  only return the value of the first matched key.
+        For more precise operations consider using get() or get_by_key_path().
+
+        I.e:
+
+            considering the following LD:
+                MainPage
+                    @type
+                    @content
+                    BreadcrumbList
+                        ...
+                        ...
+                    NewsArticle
+                        datePublished: ...
+                        authors: ...
+
+            the contents of 'MainPage' count as depth 1.
+
+            So
+                breadth_first_search('authors') -> None,
+
+            whereas
+
+                breadth_first_search('@content') -> the value of key '@content'
+
+            and
+
+                breadth_first_search('authors', 2) -> the value of key 'authors'
+
+        :param key: The dict key to search for
+        :param depth: The searched depth, default None
+        :return: The content of the first matched key or None
+        """
+
+        def search_recursive(nodes: Iterable[dict], current_depth: int):
+            if current_depth == depth:
+                return None
+            else:
+                new = []
+                for node in nodes:
+                    if isinstance(node, dict) and (value := node.get(key)):
+                        return value
+                    new.extend(v for v in node.values() if isinstance(v, dict))
+                return search_recursive(new, current_depth + 1)
+
+        return search_recursive(self._ld_by_type.values(), 0)
 
     def __repr__(self):
         return f"LD containing '{', '.join(self._contains)}'"
