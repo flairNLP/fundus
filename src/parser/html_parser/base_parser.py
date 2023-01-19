266a549f6720e82617f79ded4db5d84e0a858e31
MaxDall <46926170+MaxDall@users.noreply.github.com>
2023-01-19 17:09:13 +0100

    Little refactor for BaseParser (#37)

--- src/parser/html_parser/base_parser.py
+++ src/parser/html_parser/base_parser.py
@@ -6,30 +6,28 @@ from abc import ABC
 from collections import defaultdict
 from copy import copy
 from dataclasses import dataclass, field
-from typing import Callable, Dict, Optional, Any, Literal, List, Union, Iterable
+from typing import Callable, Dict, Optional, Any, Literal, List, Union, Tuple, Type, Iterable
 
 import lxml.html
 
 from src.parser.html_parser.utility import get_meta_content
 
 
-class RegisteredFunction:
+class RegisteredFunction(ABC):
     __wrapped__: callable = None
     __func__: callable
     __self__: object
-    __slots__ = ['__dict__', '__self__', '__func__', 'flow_type', 'priority']
+    __slots__ = ['__dict__', '__self__', '__func__', 'priority']
 
     # TODO: ensure uint for priority instead of int
     def __init__(self,
                  func: Callable,
-                 flow_type: str,
                  priority: Optional[int] = None):
 
         self.__self__ = None
         self.__func__ = func
         self.__finite__: bool = False
 
-        self.flow_type = flow_type
         self.priority = priority
 
     def __get__(self, instance, owner):
@@ -53,14 +51,32 @@ class RegisteredFunction:
 
     def __repr__(self):
         if instance := self.__self__:
-            return f"bound registered {self.flow_type} of {instance}: {self.__wrapped__} --> '{self.__name__}'"
+            return f"bound {self.__class__.__name__} of {instance}: {self.__wrapped__} --> '{self.__name__}'"
         else:
-            return f"registered {self.flow_type}: {self.__wrapped__} --> '{self.__name__}'"
+            return f"registered {self.__class__.__name__}: {self.__wrapped__} --> '{self.__name__}'"
 
 
-def _register(cls, flow_type: Literal['attribute', 'function', 'filter'], priority):
+class Attribute(RegisteredFunction):
+
+    def __init__(self,
+                 func: Callable,
+                 priority: Optional[int] = None):
+        super(Attribute, self).__init__(func=func,
+                                        priority=priority)
+
+
+class Function(RegisteredFunction):
+
+    def __init__(self,
+                 func: Callable,
+                 priority: Optional[int] = None):
+        super(Function, self).__init__(func=func,
+                                       priority=priority)
+
+
+def _register(cls, factory: Type[RegisteredFunction], priority):
     def wrapper(func):
-        return functools.update_wrapper(RegisteredFunction(func, flow_type, priority), func)
+        return functools.update_wrapper(factory(func, priority), func)
 
     # _register was called with parenthesis
     if cls is None:
@@ -70,18 +86,12 @@ def _register(cls, flow_type: Literal['attribute', 'function', 'filter'], priori
     return wrapper(cls)
 
 
-# TODO: Should 'registered_property' act like a property? and if so, implement it with a property wrapper or as __get__
-#   in 'RegisteredFunction' d
-def register_attribute(cls=None, /, *, priority: int = None):
-    return _register(cls, flow_type='attribute', priority=priority)
+def register_attribute(cls=None, /, *, priority: Optional[int] = None):
+    return _register(cls, factory=Attribute, priority=priority)
 
 
-def register_function(cls=None, /, *, priority: int = None):
-    return _register(cls, flow_type='function', priority=priority)
-
-
-def register_filter(cls=None, /, *, priority: int = None):
-    return _register(cls, flow_type='filter', priority=priority)
+def register_function(cls=None, /, *, priority: Optional[int] = None):
+    return _register(cls, factory=Function, priority=priority)
 
 
 class LinkedData:
@@ -208,8 +218,10 @@ class BaseParser(ABC):
     def __init__(self):
         self._shared_object_buffer: Dict[str, Any] = {}
 
-        self._registered_functions = [func for _, func in
-                                      inspect.getmembers(self, predicate=lambda x: isinstance(x, RegisteredFunction))]
+        predicate: callable = lambda x: isinstance(x, RegisteredFunction)
+        predicated_members: List[Tuple[str, RegisteredFunction]] = inspect.getmembers(self, predicate=predicate)
+        bound_registered_functions: List[RegisteredFunction] = [func for _, func in predicated_members]
+        self._sorted_registered_functions = sorted(bound_registered_functions, key=lambda f: (f, f.__name__))
 
         self.precomputed = Precomputed()
 
@@ -217,69 +229,64 @@ class BaseParser(ABC):
     def cache(self) -> Dict[str, Any]:
         return self.precomputed.cache
 
-    # TODO: once we have python 3.11 use getmember_static these properties
     @classmethod
-    def registered_functions(cls) -> List[RegisteredFunction]:
-        return [func for _, func in
-                inspect.getmembers(cls, predicate=lambda x: isinstance(x, RegisteredFunction))]
+    def _search_members(cls, obj_type: type) -> List[Tuple[str, Any]]:
+        members = inspect.getmembers(cls, predicate=lambda x: isinstance(x, obj_type)) if obj_type else None
+        return members
 
     @classmethod
-    def attributes(cls):
-        return [func.__name__ for func in cls.registered_functions()]
+    def attributes(cls) -> List[str]:
+        return [func.__name__ for _, func in cls._search_members(Attribute)]
 
-    def _base_setup(self):
-        content = self.precomputed.html
-        doc = lxml.html.fromstring(content)
+    def _base_setup(self, html: str) -> None:
+        self.precomputed.html = html
+        doc = lxml.html.fromstring(html)
         ld_nodes = doc.xpath("//script[@type='application/ld+json']")
         lds = [json.loads(node.text_content()) for node in ld_nodes]
         self.precomputed.doc = doc
         self.precomputed.ld = LinkedData(lds)
-        self.precomputed.meta = get_meta_content(doc) or {}
+        self.precomputed.meta = get_meta_content(doc)
+
+    def _wipe(self):
+        self.precomputed = Precomputed()
 
     def parse(self, html: str,
               error_handling: Literal['suppress', 'catch', 'raise'] = 'raise') -> Optional[Dict[str, Any]]:
 
         # wipe existing precomputed
         self._wipe()
-        self.precomputed.html = html
-        self._base_setup()
-        article_cache = {}
+        self._base_setup(html)
 
-        for func in sorted(self._registered_functions):
+        parsed_data = {}
+
+        for func in self._sorted_registered_functions:
 
             attribute_name = re.sub(r'^_{1,2}([^_]*_?)$', r'\g<1>', func.__name__)
 
-            if func.flow_type == 'function':
+            if isinstance(func, Function):
                 func()
 
-            elif func.flow_type == 'attribute':
+            elif isinstance(func, Attribute):
                 try:
-                    article_cache[attribute_name] = func()
+                    parsed_data[attribute_name] = func()
                 except Exception as err:
                     if error_handling == 'raise':
                         raise err
                     elif error_handling == 'catch':
-                        article_cache[attribute_name] = err
+                        parsed_data[attribute_name] = err
                     elif error_handling == 'suppress':
-                        article_cache[attribute_name] = None
+                        parsed_data[attribute_name] = None
                     else:
                         raise ValueError(f"Invalid value '{error_handling}' for parameter <error_handling>")
 
-            elif func.flow_type == 'filter':
-                if func():
-                    return None
-
             else:
-                raise ValueError(f'Invalid flow type {func.flow_type} for {func}')
+                raise TypeError(f"Invalid type for {func}. Only subclasses of 'RegisteredFunction' are allowed")
 
-        return article_cache
+        return parsed_data
 
     def share(self, **kwargs):
         for key, value in kwargs.items():
             self.precomputed.cache[key] = value
-
-    def _wipe(self):
-        self.precomputed = Precomputed()
 
     # base attribute section
     @register_attribute
