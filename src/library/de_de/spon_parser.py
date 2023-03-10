4740a572c9d9555046d6a0d5fc21e9884983636c
MaxDall <max.dallabetta@googlemail.com>
2023-03-10 12:41:15 +0100

    fix #78

--- src/library/de_de/spon_parser.py
+++ src/library/de_de/spon_parser.py
@@ -2,16 +2,17 @@ from datetime import datetime
 from typing import List, Optional
 
 from src.parser.html_parser import BaseParser, ArticleBody, register_attribute
-from src.parser.html_parser.utility import extract_article_body_with_css, generic_author_parsing, generic_date_parsing
+from src.parser.html_parser.utility import extract_article_body_with_selector, generic_author_parsing, \
+    generic_date_parsing
 
 
 class SPONParser(BaseParser):
     @register_attribute
     def body(self) -> ArticleBody:
-        return extract_article_body_with_css(self.precomputed.doc,
-                                             summary_selector='header .leading-loose',
-                                             subhead_selector='main .word-wrap > h3',
-                                             paragraph_selector='main .word-wrap > p')
+        return extract_article_body_with_selector(self.precomputed.doc,
+                                                  summary_selector='header .leading-loose',
+                                                  subhead_selector='main .word-wrap > h3',
+                                                  paragraph_selector='main .word-wrap > p')
 
     @register_attribute
     def authors(self) -> List[str]:
