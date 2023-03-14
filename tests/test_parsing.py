ed705166f5b6537ade47c87bddcf731080b135c1
MaxDall <max.dallabetta@googlemail.com>
2023-03-14 19:29:07 +0100

    fixed missing encoding and made functions static

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -1,8 +1,6 @@
 import json
-
 from pathlib import Path
-from typing import Dict
-
+from typing import Dict, Any
 
 import pytest
 
@@ -12,33 +10,34 @@ from src.parser.html_parser import BaseParser
 de_de = PublisherCollection.de_de
 
 
+def load_html(parser: BaseParser) -> str:
+    name = parser.__class__.__name__
+    file_source_path = Path(f"./tests/ressources/{name}.html").resolve()
+
+    with open(file_source_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+    return content
+
+
+def load_data(parser: BaseParser) -> Dict[str, Any]:
+    name = parser.__class__.__name__
+    file_source_path = Path(f"./tests/ressources/{name}.json").resolve()
+
+    with open(file_source_path, 'r', encoding='utf-8') as file:
+        content = file.read()
+
+    return json.loads(content)
+
+
 @pytest.mark.parametrize(
-    "parser", [el.parser() for el in de_de], ids=[crawler.name for crawler in de_de]
+    "parser", [publisher.parser() for publisher in de_de], ids=[publisher.name for publisher in de_de]
 )
 class TestCrawling:
-    def load_html(self, parser: BaseParser) -> str:
-        name = parser.__class__.__name__
-        file_source_path = Path(f"./tests/ressources/{name}.html").resolve()
 
-        with open(
-            file_source_path,
-        ) as file:
-            content = file.read()
-        return content
+    def test_parser(self, parser: BaseParser) -> None:
+        html = load_html(parser)
+        comparative_data = load_data(parser)
 
-    def load_data(self, parser: BaseParser) -> Dict:
-        name = parser.__class__.__name__
-        file_source_path = Path(f"./tests/ressources/{name}.json").resolve()
-
-        with open(file_source_path) as file:
-            content = file.read()
-
-        return json.loads(content)
-
-    def test_parsing(self, parser: BaseParser) -> None:
-        input = self.load_html(parser)
-        comparative_data = self.load_data(parser)
-
-        result = parser.parse(input, "raise")
-        for key_el in comparative_data.keys():
-            assert comparative_data[key_el] == result[key_el]
+        result = parser.parse(html, "raise")
+        for key in comparative_data.keys():
+            assert comparative_data[key] == result[key]
