48523e8c58dda341131867eefa42a01ba1076b39
MaxDall <max.dallabetta@googlemail.com>
2023-03-14 20:39:06 +0100

    fixed naming schema

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -5,23 +5,22 @@ from typing import Dict, Any
 import pytest
 
 from src.library.collection import PublisherCollection
+from src.library.collection.base_objects import PublisherEnum
 from src.parser.html_parser import BaseParser
 
 de_de = PublisherCollection.de_de
 
 
-def load_html(parser: BaseParser) -> str:
-    name = parser.__class__.__name__
-    file_source_path = Path(f"./tests/ressources/{name}.html").resolve()
+def load_html(publisher_name: str) -> str:
+    file_source_path = Path(f"./tests/ressources/{publisher_name}.html").resolve()
 
     with open(file_source_path, 'r', encoding='utf-8') as file:
         content = file.read()
     return content
 
 
-def load_data(parser: BaseParser) -> Dict[str, Any]:
-    name = parser.__class__.__name__
-    file_source_path = Path(f"./tests/ressources/{name}.json").resolve()
+def load_data(publisher_name: str) -> Dict[str, Any]:
+    file_source_path = Path(f"./tests/ressources/{publisher_name}.json").resolve()
 
     with open(file_source_path, 'r', encoding='utf-8') as file:
         content = file.read()
@@ -30,15 +29,16 @@ def load_data(parser: BaseParser) -> Dict[str, Any]:
 
 
 @pytest.mark.parametrize(
-    "parser",
-    [publisher.parser() for publisher in PublisherCollection],
+    "publisher",
+    list(PublisherCollection),
     ids=[publisher.name for publisher in PublisherCollection]
 )
 class TestCrawling:
 
-    def test_parser(self, parser: BaseParser) -> None:
-        html = load_html(parser)
-        comparative_data = load_data(parser)
+    def test_parser(self, publisher: PublisherEnum) -> None:
+        html = load_html(publisher.name)
+        comparative_data = load_data(publisher.name)
+        parser = publisher.parser()
 
         result = parser.parse(html, "raise")
         for key in comparative_data.keys():
