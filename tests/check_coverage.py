f7c704f5bf2b15c3af2282b07d4bbd42e290dece
aaron <aaronwey@posteo.de>
2023-03-25 10:52:06 +0100

    Black, Isort

--- tests/check_coverage.py
+++ tests/check_coverage.py
@@ -1,26 +1,22 @@
 import json
 from pathlib import Path
-from typing import List, Dict, Any
+from typing import Any, Dict, List
 
 
-def load_json()->Dict[str, Dict[str, Any]]:
-    result={}
-    for file_el in sorted([el for el in Path('./ressources').iterdir() if 'json' in el.suffix]):
-        with open(file_el, 'r', encoding='utf-8') as file:
-            result.update({file_el.name:json.load(file)})
+def load_json() -> Dict[str, Dict[str, Any]]:
+    result = {}
+    for file_el in sorted([el for el in Path("./ressources").iterdir() if "json" in el.suffix]):
+        with open(file_el, "r", encoding="utf-8") as file:
+            result.update({file_el.name: json.load(file)})
     return result
 
 
-
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     file_name_dict_dict = load_json()
 
-    required_keys =['authors', 'title', 'topics']
+    required_keys = ["authors", "title", "topics"]
 
     for key, item in file_name_dict_dict.items():
         for required_key_el in required_keys:
             if required_key_el not in item.keys():
-                print(f'{required_key_el} is missing from {key}')
-
+                print(f"{required_key_el} is missing from {key}")
