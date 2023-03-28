265168207860ba46a6cc33090404e9373187fc94
MaxDall <max.dallabetta@googlemail.com>
2023-03-28 15:10:59 +0200

    okay, I think I figured it out now

--- tests/test_parsing.py
+++ tests/test_parsing.py
@@ -9,14 +9,13 @@ import pytest
 from src.library.collection import PublisherCollection
 from src.library.collection.base_objects import PublisherEnum
 
-__location__ = os.path.dirname(os.path.realpath(__file__))
+__location__ = os.path.realpath(os.path.dirname(__file__))
 
 de_de = PublisherCollection.de_de
 
 
 def load_html(publisher_name: str) -> str:
-    print(__location__)
-    relative_resource_path = Path(f"./ressources/{publisher_name}.html.gz").resolve()
+    relative_resource_path = os.path.normpath(f"./ressources/{publisher_name}.html.gz")
     absolute_path = os.path.join(__location__, relative_resource_path)
 
     with open(absolute_path, "rb") as file:
@@ -28,7 +27,7 @@ def load_html(publisher_name: str) -> str:
 
 
 def load_data(publisher_name: str) -> Dict[str, Any]:
-    relative_resource_path = Path(f"./ressources/{publisher_name}.json").resolve()
+    relative_resource_path = os.path.normpath(Path(f"./ressources/{publisher_name}.json"))
     absolute_path = os.path.join(__location__, relative_resource_path)
 
     with open(absolute_path, "r", encoding="utf-8") as file:
