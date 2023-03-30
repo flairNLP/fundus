74b3460ad0b7cbe6f72303cfe0bbb037c16de8f7
MaxDall <max.dallabetta@googlemail.com>
2023-03-30 20:47:34 +0200

    unify en with us

--- src/library/us/__init__.py
+++ src/library/us/__init__.py
@@ -1,5 +1,6 @@
 from src.library.collection.base_objects import PublisherEnum, PublisherSpec
-from src.library.us.ap_news import APNewsParser
+from .ap_news import APNewsParser
+from .fox_news import FoxNewsParser
 
 
 class US(PublisherEnum):
@@ -8,4 +9,11 @@ class US(PublisherEnum):
         sitemaps=["https://apnews.com/sitemap/sitemaps/sitemap_index.xml"],
         news_map="https://apnews.com/sitemap/google-news-sitemap/sitemap_index.xml",
         parser=APNewsParser,
+    )
+
+    FoxNews = PublisherSpec(
+        domain="https://foxnews.com/",
+        sitemaps=[" https://www.foxnews.com/sitemap.xml"],
+        news_map='https://www.foxnews.com/sitemap.xml?type=news',
+        parser=FoxNewsParser,
     )
