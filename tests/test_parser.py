161e658afaf8652dbb96ce2a068f8e48711dc631
MaxDall <max.dallabetta@googlemail.com>
2023-03-30 04:46:01 +0200

    add annotation test case

--- tests/test_parser.py
+++ tests/test_parser.py
@@ -8,14 +8,12 @@ import pytest
 
 from src.library.collection import PublisherCollection
 from src.library.collection.base_objects import PublisherEnum
-from tests.resources import resource_dir_path
-
-de_de = PublisherCollection.de_de
+from tests.resources import attribute_annotations, parser_test_data_path
 
 
 def load_html(publisher_name: str) -> str:
     relative_file_path = Path(f"{publisher_name}.html.gz")
-    absolute_path = os.path.join(resource_dir_path, relative_file_path)
+    absolute_path = os.path.join(parser_test_data_path, relative_file_path)
 
     with open(absolute_path, "rb") as file:
         content = file.read()
@@ -27,7 +25,7 @@ def load_html(publisher_name: str) -> str:
 
 def load_data(publisher_name: str) -> Dict[str, Any]:
     relative_file_path = Path(f"{publisher_name}.json")
-    absolute_path = os.path.join(resource_dir_path, relative_file_path)
+    absolute_path = os.path.join(parser_test_data_path, relative_file_path)
 
     with open(absolute_path, "r", encoding="utf-8") as file:
         content = file.read()
@@ -42,8 +40,16 @@ def load_data(publisher_name: str) -> Dict[str, Any]:
 @pytest.mark.parametrize(
     "publisher", list(PublisherCollection), ids=[publisher.name for publisher in PublisherCollection]
 )
-class TestParsing:
-    def test_parser(self, publisher: PublisherEnum) -> None:
+class TestParser:
+    def test_annotations(self, publisher: PublisherEnum) -> None:
+        parser = publisher.parser
+        for attr in parser.attributes():
+            if annotation := attribute_annotations[attr.__name__]:
+                assert (
+                    attr.__annotations__.get("return") == annotation
+                ), f"Attribute {attr.__name__} for {parser.__name__} failed"
+
+    def test_parsing(self, publisher: PublisherEnum) -> None:
         html = load_html(publisher.name)
         comparative_data = load_data(publisher.name)
         parser = publisher.parser()
