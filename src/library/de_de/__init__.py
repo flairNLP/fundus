594336b0a75281179b489cc9992e012071fbe5f4
aaron <aaronwey@posteo.de>
2023-03-30 14:01:47 +0200

    Adds support for the waz

--- src/library/de_de/__init__.py
+++ src/library/de_de/__init__.py
@@ -13,6 +13,7 @@ from .merkur_parser import MerkurParser
 from .spon_parser import SPONParser
 from .sz_parser import SZParser
 from .tagesschau_parser import TagesschauParser
+from .waz import WazParser
 
 
 # noinspection PyPep8Naming
@@ -105,3 +106,10 @@ class DE_DE(PublisherEnum):
         news_map="https://www.dw.com/de/news-sitemap.xml",
         parser=DWParser,
     )
+
+    WAZ = PublisherSpec(
+        domain="https://www.waz.de/",
+        news_map= "https://www.waz.de/sitemaps/news.xml",
+        parser=WazParser,
+    )
+
---
+++ src/library/de_de/waz.py
@@ -0,0 +1,37 @@
+import datetime
+from typing import List, Optional
+
+from src.parser.html_parser import ArticleBody, BaseParser, attribute
+from src.parser.html_parser.utility import (
+    extract_article_body_with_selector,
+    generic_author_parsing,
+    generic_date_parsing,
+    generic_topic_parsing,
+)
+
+
+class WazParser(BaseParser):
+    @attribute
+    def body(self) -> ArticleBody:
+        return extract_article_body_with_selector(
+            self.precomputed.doc,
+            paragraph_selector=".article__body > p",
+            summary_selector='.article__header__intro__text',
+            subheadline_selector=".article__body > h3",
+        )
+
+    @attribute
+    def title(self) -> Optional[str]:
+        return self.precomputed.meta.get("og:title")
+
+    @attribute
+    def authors(self) -> List[str]:
+        return generic_author_parsing(self.precomputed.meta.get("author"))
+
+    @attribute
+    def publishing_date(self) -> Optional[datetime.datetime]:
+        return generic_date_parsing(self.precomputed.meta.get("datePublished"))
+
+    @attribute
+    def topics(self) -> Optional[List[str]]:
+        return generic_topic_parsing(self.precomputed.meta.get("keywords"))
