92b691c3a48bd0ffe96ba4732be9914a4b5b2c84
aaron <aaronwey@posteo.de>
2023-04-03 10:33:31 +0200

    Adds support for the free beacon

---
+++ src/library/us/free_beacon.py
@@ -0,0 +1,41 @@
+import re
+from datetime import datetime
+from typing import List, Optional
+
+from lxml.cssselect import CSSSelector
+from lxml.etree import XPath
+
+from src.parser.html_parser import ArticleBody, BaseParser, attribute
+from src.parser.html_parser.utility import (
+    extract_article_body_with_selector,
+    generic_author_parsing,
+    generic_date_parsing,
+    generic_topic_parsing,
+)
+
+
+class FreeBeaconParser(BaseParser):
+    _author_selector: XPath = XPath(f"{CSSSelector('div.CardHeadline').path}/span/span[1]")
+
+    @attribute
+    def body(self) -> ArticleBody:
+        return extract_article_body_with_selector(
+            self.precomputed.doc,
+            paragraph_selector=".article-content > p",
+        )
+
+    @attribute
+    def authors(self) -> List[str]:
+        return generic_author_parsing(self.precomputed.meta.get("author"))
+
+    @attribute
+    def publishing_date(self) -> Optional[datetime]:
+        return generic_date_parsing(self.precomputed.ld.bf_search("datePublished"))
+
+    @attribute
+    def title(self) -> Optional[str]:
+        return self.precomputed.ld.bf_search("headline")
+
+    @attribute
+    def topics(self) -> List[str]:
+        return self.precomputed.ld.bf_search("keywords")
