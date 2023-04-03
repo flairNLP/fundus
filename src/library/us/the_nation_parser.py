2270e61b1b5da7173e866cbf7e152adcae205549
aaron <aaronwey@posteo.de>
2023-04-03 15:11:37 +0200

    Improves the nation parser

--- src/library/us/the_nation_parser.py
+++ src/library/us/the_nation_parser.py
@@ -21,34 +21,21 @@ class TheNationParser(BaseParser):
     def body(self) -> ArticleBody:
         return extract_article_body_with_selector(
             self.precomputed.doc,
-            summary_selector="//div[@data-key = 'article']/p[1]",
-            subheadline_selector="//div[@data-key = 'article']/h2",
-            paragraph_selector="//div[@data-key = 'article']/p[position() > 1]",
-            mode="xpath",
+            paragraph_selector=".article > p"
         )
 
     @attribute
     def authors(self) -> List[str]:
-        # AP News does not have all the article's authors listed in the linked data.
-        # Therefore, we try to parse the article's authors from the document.
-        try:
-            # Example: "By AUTHOR1, AUTHOR2 and AUTHOR3"
-            author_string: str = self._author_selector(self.precomputed.doc)[0].text_content()
-            author_string = author_string[3:]  # Strip "By "
-        except IndexError:
-            # Fallback to the generic author parsing from the linked data.
-            return generic_author_parsing(self.precomputed.ld.get_value_by_key_path(["NewsArticle", "author"]))
-
-        return re.split(r"\sand\s|,\s", author_string)
+        print()
 
     @attribute
     def publishing_date(self) -> Optional[datetime]:
-        return generic_date_parsing(self.precomputed.ld.get_value_by_key_path(["NewsArticle", "datePublished"]))
+        return generic_date_parsing(self.precomputed.ld.bf_search('datePublished'))
 
     @attribute
     def title(self) -> Optional[str]:
-        title: str = self.precomputed.ld.get_value_by_key_path(["NewsArticle", "headline"])
-        return title
+        return self.precomputed.meta.get('og:title')
+
 
     @attribute
     def topics(self) -> List[str]:
