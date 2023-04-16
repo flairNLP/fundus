5c4b3b7581c28d8bb98a25b7bdf111f846839360
MaxDall <max.dallabetta@googlemail.com>
2023-04-16 21:27:10 +0200

    try to use svg

--- src/fundus/utils/generate_tables.py
+++ src/fundus/utils/generate_tables.py
@@ -1,48 +1,80 @@
 import textwrap
 from dataclasses import dataclass, field
 from pathlib import Path
-from typing import Callable, Dict, Iterator, List
+from typing import Callable, Dict, Iterator, List, Union
 from urllib.parse import urlparse
+
+from typing_extensions import TypeAlias
 
 from fundus import PublisherCollection
 from fundus import __development_base_path__ as root_path
 from fundus.publishers.base_objects import PublisherEnum
+from fundus.utils.validation import listify
+
+
+def generate_line(content: str, indent: int = 0, newline: bool = True) -> str:
+    return textwrap.indent(content, prefix="\t" * indent) + ("\n" if newline else "")
+
+
+ContentT: TypeAlias = Union[str, "Tag"]
 
 
 @dataclass
-class TableColumn:
-    content: Callable[[PublisherEnum], str]
-    styles: List[str] = field(default_factory=list)
+class Tag:
+    type: str
+    content: Union[List[ContentT], ContentT]
+    attrs: Dict[str, str] = field(default_factory=dict)
+    inline: bool = False
 
-    def __call__(self, *args, **kwargs) -> str:
-        style = f' style="{"; ".join(self.styles)}"' if self.styles else ""
-        return textwrap.dedent(f"<td{style}>{self.content(*args, **kwargs)}</td>\n")
+    def __str__(self) -> str:
+        lines: List[str] = list()
+        inline_attrs: str = "".join([f' {key}="{value}"' for key, value in self.attrs.items()])
+        lines.append(generate_line(f"<{self.type}{inline_attrs}>", newline=not self.inline))
+        if isinstance(self.content, list):
+            lines.extend(generate_line(str(c), indent=1 if not self.inline else 0, newline=False) for c in self.content)
+        elif isinstance(self.content, Tag):
+            lines.append(generate_line(str(self.content), indent=1, newline=False))
+        elif self.content:
+            lines.append(generate_line(str(self.content), indent=1, newline=not self.inline))
+        lines.append(generate_line(f"</{self.type}>"))
+        return "".join(lines)
 
 
-column_mapping: Dict[str, TableColumn] = {
-    "Source": TableColumn(
+@dataclass
+class ColumnFactory:
+    content: Callable[[PublisherEnum], ContentT]
+    style: Dict[str, str] = field(default_factory=dict)
+
+    def __call__(self, *args, **kwargs) -> Tag:
+        style = "; ".join(f"{key}: {value}" for key, value in self.style.items())
+        attrs = {"style": style}
+        return Tag(type="td", content=self.content(*args, **kwargs), attrs=attrs, inline=False)
+
+
+max_width: int = 1000
+column_mapping: Dict[str, ColumnFactory] = {
+    "Source": ColumnFactory(
         content=lambda spec: f"{spec.publisher_name}",
     ),
-    "Domain": TableColumn(
-        content=lambda spec: (
-            f'\n\t<a href="{spec.domain}">\n' f"\t\t<span>{urlparse(spec.domain).netloc}</span>\n" f"\t</a>\n"
-        )
+    "Domain": ColumnFactory(
+        content=lambda spec: Tag("a", Tag("span", urlparse(spec.domain).netloc), {"href": spec.domain})
     ),
-    "Validated Attributes": TableColumn(
-        content=lambda spec: "<code>" + str(spec.parser.attributes().validated) + "</code>"
-    ),
-    "Unvalidated Attributes": TableColumn(
-        content=lambda spec: "<code>" + str(spec.parser.attributes().unvalidated) + "</code>"
+    "Validated Attributes": ColumnFactory(content=lambda spec: Tag("code", str(spec.parser.attributes().validated))),
+    "Unvalidated Attributes": ColumnFactory(
+        content=lambda spec: Tag("code", str(spec.parser.attributes().unvalidated))
         if spec.parser.attributes().unvalidated
         else ""
     ),
-    "Class": TableColumn(content=lambda spec: f"<code>{spec.name}</code>"),
+    "Class": ColumnFactory(content=lambda spec: Tag("code", spec.name)),
 }
 
+# column_style = {"text-align": "center"}
+# for column in column_mapping.values():
+#     column.style.update(column_style)
 
-def generate_style() -> str:
-    style: str = f"""
-        <style>
+
+def generate_style() -> Tag:
+    style_content: str = f"""
             .source {{
                 text-align: center;
             }}
@@ -54,65 +86,63 @@ def generate_style() -> str:
             }}
             .source tr > *:last-of-type {{
                 text-align: right;
-            }}
-        </style>"""
-    return textwrap.dedent(style.strip("\n"))
+            }}"""
+    style = Tag("style", textwrap.dedent(style_content.strip("\n")))
+    return style
 
 
-def generate_thread() -> str:
-    thread: List[str] = list()
-    thread.append(generate_line("<thread>"))
-    thread.append(generate_line("<tr>", indent=+1))
-    for key in column_mapping.keys():
-        thread.append(generate_line(f"<th>{key}</th>", indent=+2))
-    thread.append(generate_line("</tr>", indent=+1))
-    thread.append(generate_line("</thread>"))
-    return "".join(thread)
+def generate_thread() -> Tag:
+    ths = [Tag("th", name, inline=False) for name in column_mapping.keys()]
+    tr = Tag("tr", ths)
+    thread = Tag("thread", tr)
+    return thread
 
 
-def generate_tbody_tr(spec: PublisherEnum) -> str:
-    tr: List[str] = list()
-    tr.append(generate_line("<tr>"))
-    for column in column_mapping.values():
-        tr.append(textwrap.indent(column(spec), prefix="\t"))
-    tr.append(generate_line("</tr>"))
-    return "".join(tr)
-
-
-def generate_tbody(country: Iterator[PublisherEnum]) -> str:
-    tbody: List[str] = list()
-    tbody.append(generate_line("<tbody>"))
+def generate_tbody(country: Iterator[PublisherEnum]) -> Tag:
+    tbody = Tag("tbody", list())
     for spec in country:
-        tbody.append(textwrap.indent(generate_tbody_tr(spec), prefix="\t"))
-    tbody.append(generate_line("</tbody>"))
-    return "".join(tbody)
+        tds = [column(spec) for column in column_mapping.values()]
+        tr = Tag("tr", tds)
+        tbody.content.append(tr)
+    return tbody
 
 
-def build_supported_news_table() -> str:
-    table: List[str] = list()
-    table.append(generate_line("# Supported Sources"))
-    table.append(generate_style())
+def build_supported_news_svg() -> str:
+    md: List[str] = ["# Supported News Tables\n\n"]
+
+    div_content: List[Tag] = [generate_style()]
     for cc, enum in PublisherCollection.iter_countries():
-        table.append(generate_line(f"\n## {cc}"))
-        table.append(generate_line(f'<table class="source {cc}">'))
-        table.append(textwrap.indent(generate_thread(), prefix="\t"))
-        table.append(textwrap.indent(generate_tbody(enum), prefix="\t"))
-        table.append(generate_line("</table>"))
-    return "".join(table)
+        div_content.append(Tag("h2", f"{cc.upper()}-News"))
+        table = Tag(
+            "table",
+            [generate_thread(), generate_tbody(enum)],
+            attrs={
+                "class": f"source {cc}",
+            },
+        )
+        div_content.append(table)
+    div = Tag("div", div_content, attrs={"xmlns": "http://www.w3.org/1999/xhtml"})
+    fo = Tag("foreignObject", div, attrs={"width": "100%", "height": "100%"})
+    svg_attrs = {
+        "fill": "none",
+        "viewBox": "0 0 400 400",
+        "width": "400",
+        "height": "400",
+        "xmlns": "http://www.w3.org/2000/svg",
+    }
+    svg = Tag("svg", fo, attrs=svg_attrs)
 
-
-def generate_line(content: str, indent: int = 0, newline: bool = True) -> str:
-    return "\t" * indent + content + ("\n" if newline else "")
+    return str(svg)
 
 
 if __name__ == "__main__":
-    table = build_supported_news_table()
+    md = build_supported_news_svg()
 
-    relative_path = Path("doc/supported_news.md")
+    relative_path = Path("doc/supported_news.svg")
     supported_news_path = root_path / relative_path
 
     with open(supported_news_path, "w+", encoding="utf8") as file:
-        file.write(table)
+        file.write(md)
 
     import subprocess
 
