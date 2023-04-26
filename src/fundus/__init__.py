5d22383f77f0aea7f07d2a01165dd70208e19ec9
MaxDall <max.dallabetta@googlemail.com>
2023-04-26 21:11:00 +0200

    expose Required class

--- src/fundus/__init__.py
+++ src/fundus/__init__.py
@@ -2,8 +2,9 @@ import pathlib
 
 from fundus.publishers import PublisherCollection
 from fundus.scraping.pipeline import Crawler, Pipeline
+from fundus.scraping.scraper import Required
 
-__all__ = ["Crawler", "Pipeline", "PublisherCollection"]
+__all__ = ["Crawler", "Pipeline", "PublisherCollection", "Required"]
 
 __module_path__ = pathlib.Path(__file__).parent
 __development_base_path__ = __module_path__.parents[1]
