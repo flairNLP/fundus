f6dc4199f5f5d0f689de209f6e0c739d3dda83e5
aaron <aaronwey@posteo.de>
2023-05-13 09:38:11 +0200

    Fixes forgotten parameters

--- src/fundus/scraping/pipeline.py
+++ src/fundus/scraping/pipeline.py
@@ -15,10 +15,10 @@ class Pipeline:
         self.scrapers: Tuple[Scraper, ...] = scrapers
 
     def run(
-        self,
-        error_handling: Literal["suppress", "catch", "raise"],
-        max_articles: Optional[int] = None,
-        batch_size: int = 10,
+            self,
+            error_handling: Literal["suppress", "catch", "raise"],
+            max_articles: Optional[int] = None,
+            batch_size: int = 10,
     ) -> Iterator[Article]:
         scrape_map = map(lambda x: x.scrape(error_handling=error_handling, batch_size=batch_size), self.scrapers)
         robin = more_itertools.interleave_longest(*tuple(scrape_map))
@@ -42,12 +42,12 @@ class Crawler:
         self.publishers: Set[PublisherEnum] = set(more_itertools.flatten(nested_publisher))
 
     def crawl(
-        self,
-        max_articles: Optional[int] = None,
-        restrict_sources_to: Optional[Literal["rss", "sitemap", "news"]] = None,
-        error_handling: Literal["suppress", "catch", "raise"] = "suppress",
-        only_complete: Union[bool, ExtractionFilter] = True,
-        batch_size: int = 10,
+            self,
+            max_articles: Optional[int] = None,
+            restrict_sources_to: Optional[Literal["rss", "sitemap", "news"]] = None,
+            error_handling: Literal["suppress", "catch", "raise"] = "suppress",
+            only_complete: Union[bool, ExtractionFilter] = True,
+            batch_size: int = 10,
     ) -> Iterator[Article]:
         extraction_filter: Optional[ExtractionFilter]
         if isinstance(only_complete, bool):
@@ -73,7 +73,8 @@ class Crawler:
             if restrict_sources_to == "sitemap" or restrict_sources_to is None:
                 sources_from_url_sources.extend(
                     [
-                        SitemapSource(sitemap.url, publisher=spec.name)
+                        SitemapSource(sitemap.url, publisher=spec.name, reverse=sitemap.reverse,
+                                      recursive=sitemap.recursive)
                         for sitemap in spec.sources
                         if isinstance(sitemap, Sitemap)
                     ]
