eaffc52c644624ae5dfcf50baa32dd09d9ce1849
aaron <aaronwey@posteo.de>
2023-05-14 09:41:50 +0200

    Fixes mypy and black

--- src/fundus/user_feedback.py
+++ src/fundus/user_feedback.py
@@ -1,32 +1,33 @@
-from typing import Iterator
+from typing import Iterator, Optional, Set
 
 from fundus.scraping.article import Article
 
 
 class StatusDisplay(Iterator[Article]):
-
     def __init__(self, stepsize: int = 500):
         self.counter = 0
-        self.iterator = None
-        self.unique_sources = set()
+        self.iterator: Optional[Iterator[Article]] = None
+        self.unique_sources: Set[str] = set()
         self.stepsize: int = stepsize
 
     def __next__(self) -> Article:
-        current_article = next(self.iterator)
+        assert self.iterator is not None
+        current_article: Article = next(self.iterator)
         self.counter += 1
-        self.unique_sources.add(current_article.source.publisher)
+        if current_article.source.publisher:
+            self.unique_sources.add(current_article.source.publisher)
         if self.counter % self.stepsize == 0:
-            print(f'{self.counter} articles from {len(self.unique_sources)} Sources processed')
+            print(f"{self.counter} articles from {len(self.unique_sources)} Sources processed")
         return current_article
 
     def summary(self):
-        return f'{self.counter} articles from {len(self.unique_sources)} Sources processed'
+        return f"{self.counter} articles from {len(self.unique_sources)} Sources processed"
 
     def __call__(self, article_iterator: Iterator[Article]) -> Iterator[Article]:
         self.iterator = article_iterator
         return self
 
-    def __enter__(self) -> 'StatusDisplay':
+    def __enter__(self) -> "StatusDisplay":
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
