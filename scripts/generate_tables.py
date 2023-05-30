107b37a7e668b48cfccdba614c70e6dd998db336
Conrad Dobberstein <29147025+dobbersc@users.noreply.github.com>
2023-05-30 18:03:53 +0200

    Move `Class` column to the front

--- scripts/generate_tables.py
+++ scripts/generate_tables.py
@@ -22,6 +22,7 @@ class ColumnFactory(Protocol):
 
 
 column_mapping: Dict[str, ColumnFactory] = {
+    "Class": lambda spec: TD(CODE(spec.name)),
     "Source": lambda spec: TD(DIV(f"{spec.publisher_name}")),
     "URL": lambda spec: TD(A(SPAN(urlparse(spec.domain).netloc), href=spec.domain)),
     "Missing Attributes": lambda spec: TD(*[CODE(a) for a in sorted(attributes)])
@@ -33,7 +34,6 @@ column_mapping: Dict[str, ColumnFactory] = {
     "Additional Attributes": lambda spec: TD(*[CODE(a) for a in sorted(attributes)])
     if (attributes := spec.parser.latest_version.attributes().unvalidated.names)
     else cast(lxml.html.HtmlElement, lxml.html.fromstring("<td>&nbsp;</td>")),
-    "Class": lambda spec: TD(CODE(spec.name)),
 }
 
 
