8fd10c3aeabfbf94e3cacf4a721878b81607e8cb
MaxDall <max.dallabetta@googlemail.com>
2023-06-05 19:35:08 +0200

    add doc strings

--- tests/utility.py
+++ tests/utility.py
@@ -13,26 +13,68 @@ from fundus.publishers.base_objects import PublisherEnum
 from scripts.generate_tables import supported_publishers_markdown_path
 from tests.resources.parser.test_data import __module_path__ as test_resource_path
 
-T = TypeVar("T")
+_T = TypeVar("_T")
 
 
 @dataclass
-class JSONFile(Generic[T]):
+class JSONFile(Generic[_T]):
+    """Generic file class representing a JSON file.
+
+    You can specify custom json.JSONEncoder/json.JSONDecoder and type hint
+    expected JSON structure.
+
+    Example:
+        >>> class CustomEncoder(json.JSONEncoder): ...
+        >>> class CustomDecoder(json.JSONDecoder): ...
+        >>> path_to_json = Path("path/to.json")
+        >>> json_file: JSONFile[Dict[str, list]] = JSONFile(path_to_json, encoder=CustomEncoder, decoder=CustomDecoder)
+        >>> content = json_file.load() # will type hint content as Dict[str, list]
+        >>> content["entry"].append("new")
+        >>> json_file.write(content)
+    """
+
     path: Path
     encoder: Optional[Type[json.JSONEncoder]] = None
     decoder: Optional[Type[json.JSONDecoder]] = None
     encoding: str = "utf-8"
 
-    def load(self, **kwargs) -> Optional[T]:
+    def load(self, **kwargs) -> Optional[_T]:
+        """Load file content using json.load().
+
+        See the documentation of json.load() for further documentation
+        about the keyword arguments (**kwargs).
+        https://docs.python.org/3/library/json.html#json.load
+
+
+        Args:
+            **kwargs: Key word arguments for json.load()
+
+        Returns:
+            File content as if loaded with json.load(). If JSONFile is type hinted
+                properly, the expected return type is _T.
+        """
         if not self.path.exists():
             return None
         if not kwargs.get("cls"):
             kwargs["cls"] = self.decoder
         with open(self.path, "r", encoding=self.encoding) as json_file:
-            content: T = json.load(json_file, **kwargs)
+            content: _T = json.load(json_file, **kwargs)
         return content
 
-    def write(self, content: T, **kwargs) -> None:
+    def write(self, content: _T, **kwargs) -> None:
+        """Writes the given content to the file with json.dump()
+
+        See the documentation of json.dump() for further documentation
+        about the keyword arguments (**kwargs).
+        https://docs.python.org/3/library/json.html#json.dump
+
+        Args:
+            content: The content to write to the file.
+            **kwargs: Keyword arguments for json.dump()
+
+        Returns:
+
+        """
         if not kwargs:
             kwargs = {"ensure_ascii": False, "indent": 2}
         if not kwargs.get("cls"):
@@ -66,13 +108,25 @@ class ExtractionDecoder(json.JSONDecoder):
 
 
 @dataclass
-class JSONFileWithExtractionDecoderEncoder(JSONFile[T]):
+class JSONFileWithExtractionDecoderEncoder(JSONFile[_T]):
+    """Custom JSONFile using default ExtractionEncoder/ExtractionDecoder"""
+
     encoder: Type[json.JSONEncoder] = ExtractionEncoder
     decoder: Type[json.JSONDecoder] = ExtractionDecoder
 
 
 @dataclass
 class HTMLTestFile:
+    """Utility class representing an HTML test case file with meta infos attached.
+
+    When used with default constructor, writing the file will automatically generate
+    a file name and path where to write the content and register meta information about
+    the file to the corresponding meta.info file.
+
+    When used with alternative constructor HTMLTestFile.load(), the meta information will
+    be read the info file automatically.
+    """
+
     url: str
     content: str
     crawl_date: datetime.datetime
@@ -103,14 +157,7 @@ class HTMLTestFile:
     def load(cls, path: Path, encoding: str = "utf-8") -> Self:
         """Loads an HTMLTestFile from the given path.
 
-        The file at the location is expected to be a gzipped HTML file. The
-        path syntax is defined as following:
-            test_resource_path / <country_code> / <publisher_name>_<year>_<month>_<day>.html.gz
-            with:
-                <country_code>:         2-letter code, i.e. us for United States
-                <publisher_name>:       the enum name, i.e. DW, FAZ, APNews
-                <year>_<month>_<day>:   parsed date object, as when using date.strftime('%Y_%m_%d')
-
+        See write() for more information about the path syntax.
 
         Args:
             path:       The HTMLTestFile location
@@ -129,13 +176,40 @@ class HTMLTestFile:
             raise ValueError(f"Missing meta info for file '{path.name}'")
         return cls(content=content, publisher=publisher, encoding=encoding, **meta_info[path.name])
 
-    def _register_at_meta_info(self):
+    def _register_at_meta_info(self) -> None:
+        """Writes meta information about the file to the corresponding meta.info file.
+
+        Returns:
+            None
+        """
         meta_info_file = get_meta_info_file(self.publisher)
         meta_info = meta_info_file.load() or {}
         meta_info[self.path.name] = {"url": self.url, "crawl_date": self.crawl_date}
         meta_info_file.write(meta_info, default=str)
 
     def write(self) -> None:
+        """Writes the test file to an autogenerated path.
+
+        This function writes self.content to an autogenerated path and registers additional
+        meta information about the test file to the corresponding meta.info file.
+
+        You can find the corresponding meta.info file under::
+
+            test_resource_path / <country_code> / meta.info
+
+        The file path syntax is defined as following::
+
+            test_resource_path / <country_code> / <publisher_name>_<year>_<month>_<day>.html.gz
+
+        with:
+            | <country_code>: 2-letter code, e.g. us for United States were the publisher originates
+            | <publisher_name>: the enum name, e.g. DW, FAZ, APNews
+            | <year>_<month>_<day>: parsed date object, as when using date.strftime('%Y_%m_%d')
+
+        Returns:
+            None
+
+        """
         with open(self.path, "wb") as file:
             file.write(gzip.compress(bytes(self.content, self.encoding)))
         self._register_at_meta_info()
