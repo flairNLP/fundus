62f767eabd5e00f624e821658fa09561f2abea7b
MaxDall <max.dallabetta@googlemail.com>
2023-07-10 15:26:19 +0200

    GH261: fix a bug with event loop policy using windows

--- src/fundus/__init__.py
+++ src/fundus/__init__.py
@@ -1,3 +1,4 @@
+import os
 import pathlib
 
 from fundus.logging import basic_logger
@@ -10,3 +11,10 @@ __module_path__ = pathlib.Path(__file__).parent
 __development_base_path__ = __module_path__.parents[1]
 
 __all__ = ["Crawler", "BaseCrawler", "PublisherCollection", "Requires", "RSSFeed", "Sitemap", "NewsMap"]
+
+# event loop policy bug on windows, see
+# https://stackoverflow.com/questions/45600579/asyncio-event-loop-is-closed-when-getting-loop
+if os.name == "nt":
+    import asyncio
+
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
