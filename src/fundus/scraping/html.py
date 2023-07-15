8fa80aecee3b23b4f7e4932784b4de65886373f0
MaxDall <max.dallabetta@googlemail.com>
2023-07-15 15:18:31 +0200

    makes `HTMLSource` non blocking

--- src/fundus/scraping/html.py
+++ src/fundus/scraping/html.py
@@ -194,14 +194,16 @@ class HTMLSource:
     def _filter(self, url: str) -> bool:
         return any(url_filter(url) for url_filter in self.url_filter)
 
-    async def fetch(self) -> AsyncIterator[HTML]:
+    async def fetch(self) -> AsyncIterator[Optional[HTML]]:
         async for url in self.url_source:
             if not validators.url(url):
                 basic_logger.debug(f"Skipped requested URL '{url}' because the URL is malformed")
+                yield None
                 continue
 
             if self._filter(url):
                 basic_logger.debug(f"Skipped requested URL '{url}' because of URL filter")
+                yield None
                 continue
 
             session = await session_handler.get_session()
@@ -209,6 +211,7 @@ class HTMLSource:
             async with session.get(url, headers=self.request_header) as response:
                 if self._filter(str(response.url)):
                     basic_logger.debug(f"Skipped responded URL '{url}' because of URL filter")
+                    yield None
                     continue
 
                 try:
@@ -217,12 +220,14 @@ class HTMLSource:
 
                 except (HTTPError, ClientError, HttpProcessingError, UnicodeError) as error:
                     basic_logger.info(f"Skipped requested URL '{url}' because of '{error}'")
+                    yield None
                     continue
 
                 except Exception as error:
                     basic_logger.warning(
                         f"Warning! Skipped  requested URL '{url}' because of an unexpected error {error}"
                     )
+                    yield None
                     continue
 
                 if response.history:
