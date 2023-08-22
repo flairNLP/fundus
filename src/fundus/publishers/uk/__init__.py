4e21339cf1770931a0968a0bf4cdb4621a8a5333
Susanna Ruecker <51824728+susannaruecker@users.noreply.github.com>
2023-08-22 12:47:29 +0200

    Add parser for UK newspaper The Independent (#305)
    * add parser for UK newspaper The Independent
    
    * add parser and run tests for UK newspaper The Independent

--- src/fundus/publishers/uk/__init__.py
+++ src/fundus/publishers/uk/__init__.py
@@ -1,7 +1,8 @@
 from fundus.publishers.base_objects import PublisherEnum, PublisherSpec
-from fundus.scraping.html import NewsMap
+from fundus.scraping.html import NewsMap, Sitemap
 
 from .the_guardian import TheGuardianParser
+from .the_independent import TheIndependentParser
 
 
 class UK(PublisherEnum):
@@ -10,4 +11,14 @@ class UK(PublisherEnum):
         domain="https://www.theguardian.com/",
         sources=[NewsMap("https://www.theguardian.com/sitemaps/news.xml")],
         parser=TheGuardianParser,
+    )
+
+    TheIndependent = PublisherSpec(
+        name="The Independent",
+        domain="https://www.independent.co.uk/",
+        sources=[
+            Sitemap("https://www.independent.co.uk/sitemap.xml"),
+            NewsMap("https://www.independent.co.uk/sitemaps/googlenews"),
+        ],
+        parser=TheIndependentParser,
     )
