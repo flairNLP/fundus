dd70ae9b3aaccf084e4e494136c90f5e2bd08550
Alan Akbik <alan.akbik@gmail.com>
2023-09-01 15:09:29 +0200

    Add parser for LeMonde and French PublisherCollection

---
+++ src/fundus/publishers/fr/__init__.py
@@ -0,0 +1,13 @@
+from fundus.publishers.base_objects import PublisherEnum, PublisherSpec
+from fundus.publishers.fr.le_monde import LeMondeParser
+from fundus.scraping.filter import inverse, regex_filter
+from fundus.scraping.html import NewsMap, Sitemap
+
+
+class FR(PublisherEnum):
+    LeMonde = PublisherSpec(
+        name="Le Monde",
+        domain="https://www.lemonde.fr/",
+        sources=[NewsMap("https://www.lemonde.fr/sitemap_news.xml", reverse=True)],
+        parser=LeMondeParser,
+    )
