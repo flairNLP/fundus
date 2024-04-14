7c43cd2d815fe9f1ca570a028c8889d755120e2a
Adrian Breiding <ad123br@gmail.com>
2024-04-14 21:33:19 +0200

    change mypy type declaration (test)

--- src/fundus/publishers/de/faz.py
+++ src/fundus/publishers/de/faz.py
@@ -6,6 +6,7 @@ from lxml.html import HtmlElement
 
 from fundus.parser import ArticleBody, BaseParser, ParserProxy, attribute
 from fundus.parser.utility import (
+    Node,
     extract_article_body_with_selector,
     generic_date_parsing,
     generic_topic_parsing,
@@ -45,7 +46,7 @@ class FAZParser(ParserProxy):
                 if len(author_nodes) > 1:
                     # With more than one entry, we abuse the fact that authors are linked with an <a> tag,
                     # but cities are not
-                    node: HtmlElement
+                    node: Node
                     author_nodes = [node for node in author_nodes if next(node.iterchildren(tag="a"), None) is not None]
                 return [text for node in author_nodes if "F.A.Z" not in (text := node.text_content())]
 
