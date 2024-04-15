1af05bb1610f7c010cf5ad382b23adcb15fe188e
Max Dallabetta <max.dallabetta@googlemail.com>
2024-04-15 16:41:28 +0200

    add `RheinischePost`

--- src/fundus/publishers/de/__init__.py
+++ src/fundus/publishers/de/__init__.py
@@ -19,6 +19,7 @@ from .mdr import MDRParser
 from .merkur import MerkurParser
 from .ndr import NDRParser
 from .ntv import NTVParser
+from .rheinische_post import RheinischePostParser
 from .spon import SPONParser
 from .stern import SternParser
 from .sz import SZParser
@@ -226,4 +227,11 @@ class DE(PublisherEnum):
             Sitemap("https://www.businessinsider.de/sitemap_index.xml"),
         ],
         parser=BusinessInsiderDEParser,
+    )
+
+    RheinischePost = PublisherSpec(
+        name="Rheinische Post",
+        domain="https://rp-online.de/",
+        sources=[NewsMap("https://rp-online.de/sitemap-news.xml"), Sitemap("https://rp-online.de/sitemap.xml")],
+        parser=RheinischePostParser,
     )
