a65d761dbc79d411dd5db3ba8b701b9f9d73e050
ozelalisen <ozelalisen@gmail.com>
2024-04-29 17:05:00 +0200

    [fix]: sitemap filter added & parser selectors adjusted

--- src/fundus/publishers/tr/__init__.py
+++ src/fundus/publishers/tr/__init__.py
@@ -1,4 +1,5 @@
 from fundus.publishers.base_objects import PublisherEnum, PublisherSpec
+from fundus.scraping.filter import inverse, regex_filter
 from fundus.scraping.url import NewsMap, Sitemap
 
 from .haberturk import HaberturkParser
@@ -9,7 +10,11 @@ class TR(PublisherEnum):
         name="Haberturk",
         domain="https://www.haberturk.com/",
         sources=[
-            Sitemap("https://www.haberturk.com/sitemap.xml"),
+            Sitemap(
+                "https://www.haberturk.com/sitemap.xml",
+                sitemap_filter=inverse(regex_filter("news|special|posts|ozel_icerikler")),
+                reverse=True,
+            ),
             NewsMap("https://www.haberturk.com/sitemap_google_news.xml"),
         ],
         parser=HaberturkParser,
