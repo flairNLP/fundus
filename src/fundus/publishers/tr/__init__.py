dfa2d4f29710f9f565c6a6ab101f3a8b7fbfe497
Max Dallabetta <max.dallabetta@googlemail.com>
2024-04-30 13:35:25 +0200

    add `sitemap`

--- src/fundus/publishers/tr/__init__.py
+++ src/fundus/publishers/tr/__init__.py
@@ -1,5 +1,6 @@
 from fundus.publishers.base_objects import PublisherEnum, PublisherSpec
 from fundus.publishers.tr.ntvtr import NTVTRParser
+from fundus.scraping.filter import regex_filter
 from fundus.scraping.url import NewsMap, RSSFeed, Sitemap
 
 
@@ -11,5 +12,6 @@ class TR(PublisherEnum):
         sources=[
             RSSFeed("https://www.ntv.com.tr/gundem.rss"),
             NewsMap("https://www.ntv.com.tr/sitemaps/news-sitemap.xml"),
+            Sitemap("https://www.ntv.com.tr/sitemaps", sitemap_filter=regex_filter("news-sitemap.xml")),
         ],
     )
