07a049607fefabd76efcdb501020bd40475e535e
Umut yeşildal <umutyesildal97@gmail.com>
2024-05-01 22:10:16 +0200

    deleted test :)

--- src/fundus/publishers/test.py
+++
@@ -1,9 +0,0 @@
-from fundus.publishers import PublisherCollection
-from fundus.scraping.crawler import Crawler
-
-publisher = PublisherCollection.tr.NTVTR
-
-crawler = Crawler(publisher)
-
-for article in crawler.crawl(max_articles=2, only_complete=False):
-    print(article)
