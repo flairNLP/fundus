63c79f44fbea1aad4f125d2d4fc8321bff0d15fc
Max Dallabetta <max.dallabetta@googlemail.com>
2024-05-02 21:10:57 +0200

    finish parser

--- src/fundus/publishers/de/gamestar.py
+++ src/fundus/publishers/de/gamestar.py
@@ -8,12 +8,15 @@ from fundus.parser.utility import (
     extract_article_body_with_selector,
     generic_author_parsing,
     generic_date_parsing,
+    generic_topic_parsing,
 )
 
 
 class GamestarParser(ParserProxy):
     class V1(BaseParser):
-        _paragraph_selector = CSSSelector("div.article-content > p")
+        _summary_selector = CSSSelector("p.intro")
+        _paragraph_selector = CSSSelector("div.article-content > p:not([class])")
+        _subheadline_selector = CSSSelector("div.article-content > h2")
 
         @attribute
         def title(self) -> Optional[str]:
@@ -24,6 +27,8 @@ class GamestarParser(ParserProxy):
             return extract_article_body_with_selector(
                 self.precomputed.doc,
                 paragraph_selector=self._paragraph_selector,
+                summary_selector=self._summary_selector,
+                subheadline_selector=self._subheadline_selector,
             )
 
         @attribute
@@ -33,11 +38,3 @@ class GamestarParser(ParserProxy):
         @attribute
         def publishing_date(self) -> Optional[datetime]:
             return generic_date_parsing(self.precomputed.ld.bf_search("datePublished"))
-
-        @attribute
-        def topics(self) -> List[str]:
-            keywords = self.precomputed.meta.get("keywords")
-            if not keywords:
-                return [""]
-            else:
-                return keywords.strip(" ").split(",")
