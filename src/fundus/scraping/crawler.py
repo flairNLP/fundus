c73c40252eccfd9b9c38a8668dc9ac934bf714d9
Max Dallabetta <max.dallabetta@googlemail.com>
2024-05-03 16:11:22 +0200

    rename function

--- src/fundus/scraping/crawler.py
+++ src/fundus/scraping/crawler.py
@@ -118,7 +118,7 @@ def pool_queue_iter(handle: MapResult[Any], queue: Queue[_T]) -> Iterator[_T]:
             return
 
 
-def remove_parameters_from_url(url: str) -> str:
+def remove_query_parameters_from_url(url: str) -> str:
     if any(parameter_indicator in url for parameter_indicator in ("?", "#")):
         return urljoin(url, urlparse(url).path)
     return url
@@ -219,7 +219,7 @@ class CrawlerBase(ABC):
         for article in self._build_article_iterator(
             tuple(fitting_publishers), error_handling, build_extraction_filter(), url_filter
         ):
-            url_without_query_parameters = remove_parameters_from_url(article.html.responded_url)
+            url_without_query_parameters = remove_query_parameters_from_url(article.html.responded_url)
             if not only_unique or url_without_query_parameters not in response_cache:
                 response_cache.add(url_without_query_parameters)
                 article_count += 1
