b1282a0969f2ba61620e694af1757fd50fb202e5
Adrian Breiding <ad123br@gmail.com>
2024-05-03 18:51:46 +0200

    Finish Up MalayMail

--- src/fundus/publishers/my/malay_mail.py
+++ src/fundus/publishers/my/malay_mail.py
@@ -2,24 +2,29 @@ import datetime
 from typing import List, Optional
 
 from lxml.cssselect import CSSSelector
+from lxml.etree import XPath
 
 from fundus.parser import ArticleBody, BaseParser, ParserProxy, attribute
 from fundus.parser.utility import (
     extract_article_body_with_selector,
     generic_author_parsing,
     generic_date_parsing,
+    generic_topic_parsing,
 )
 
 
 class MalayMailParser(ParserProxy):
     class V1(BaseParser):
-        _paragraph_selector = CSSSelector("div.article-body > p")
+        # Use negated _subheadline_selector as _paragraph_selector to ensure no double parsing
+        _paragraph_selector = XPath("//div[@class='article-body']/p[text() or not(b)]")
+        _subheadline_selector = XPath("//div[@class='article-body']/p[not(text()) and b]")
 
         @attribute
         def body(self) -> ArticleBody:
             return extract_article_body_with_selector(
                 self.precomputed.doc,
                 paragraph_selector=self._paragraph_selector,
+                subheadline_selector=self._subheadline_selector,
             )
 
         @attribute
@@ -33,3 +38,7 @@ class MalayMailParser(ParserProxy):
         @attribute
         def title(self) -> Optional[str]:
             return self.precomputed.meta.get("og:title")
+
+        @attribute
+        def topics(self) -> List[str]:
+            return generic_topic_parsing(self.precomputed.meta.get("keywords"))
