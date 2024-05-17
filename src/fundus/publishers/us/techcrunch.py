7f9e01f8764635c79e055101f788fa34e91a8bea
Max Dallabetta <max.dallabetta@googlemail.com>
2024-05-17 09:20:33 +0200

    update `TechCrunch` to version 2 and fix an extraction bug for version 1

--- src/fundus/publishers/us/techcrunch.py
+++ src/fundus/publishers/us/techcrunch.py
@@ -14,10 +14,42 @@ from fundus.parser.utility import (
 
 
 class TechCrunchParser(ParserProxy):
+    class V2(BaseParser):
+        _summary_selector: XPath = CSSSelector("div.entry-content > p#speakable-summary")
+        _paragraph_selector: XPath = CSSSelector("div.entry-content > p:not(#speakable-summary)")
+        _subheadline_selector: CSSSelector = CSSSelector("div.entry-content > h2")
+
+        @attribute
+        def body(self) -> ArticleBody:
+            body: ArticleBody = extract_article_body_with_selector(
+                self.precomputed.doc,
+                subheadline_selector=self._subheadline_selector,
+                summary_selector=self._summary_selector,
+                paragraph_selector=self._paragraph_selector,
+            )
+            return body
+
+        @attribute
+        def authors(self) -> List[str]:
+            return generic_author_parsing(self.precomputed.meta.get("author"))
+
+        @attribute
+        def publishing_date(self) -> Optional[datetime.datetime]:
+            return generic_date_parsing(self.precomputed.ld.bf_search("datePublished"))
+
+        @attribute
+        def title(self) -> Optional[str]:
+            return self.precomputed.meta.get("og:title")
+
+        @attribute
+        def topics(self) -> List[str]:
+            return generic_topic_parsing(self.precomputed.ld.bf_search("keywords"))
+
     class V1(BaseParser):
+        VALID_UNTIL = datetime.date(2024, 5, 8)
         _summary_selector: XPath = CSSSelector("div.article-content > p#speakable-summary")
-        _paragraph_selector: XPath = CSSSelector(".article-content > p")
-        _subheadline_selector: CSSSelector = CSSSelector(".article-content > h2")
+        _paragraph_selector: XPath = CSSSelector("div.article-content > p:not(#speakable-summary)")
+        _subheadline_selector: CSSSelector = CSSSelector("div.article-content > h2")
 
         @attribute
         def body(self) -> ArticleBody:
@@ -35,7 +67,6 @@ class TechCrunchParser(ParserProxy):
 
         @attribute
         def publishing_date(self) -> Optional[datetime.datetime]:
-            # return generic_date_parsing(self.precomputed.ld.get_value_by_key_path(["NewsArticle", "datePublished"]))
             return generic_date_parsing(self.precomputed.ld.bf_search("datePublished"))
 
         @attribute
