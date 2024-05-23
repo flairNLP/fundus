d3d1396dfd6625761bdcc886271ac1debc4996ed
Adrian Breiding <ad123br@gmail.com>
2024-05-23 16:15:51 +0200

    delete other leftover files

--- src/fundus/publishers/collections.py
+++
@@ -1,8 +0,0 @@
-from fundus.publishers.base_objects import PublisherGroup
-
-
-class AllPublishers(PublisherGroup):
-    test = 0
-
-    def test2(self):
-        return 0
--- src/fundus/publishers/collections.pyi
+++
@@ -1,5 +0,0 @@
-from fundus.publishers.base_objects import Publisher
-
-class AllPublishers:
-    Tagesschau: Publisher
-    TheNamibian: Publisher
