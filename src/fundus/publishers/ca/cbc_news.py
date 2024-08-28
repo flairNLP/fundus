4371d6b77d04b93e9da3d9698614421bb188719f
Max Dallabetta <max.dallabetta@googlemail.com>
2024-08-28 16:59:13 +0200

    rework `CBCNews` parser

--- src/fundus/publishers/ca/cbc_news.py
+++ src/fundus/publishers/ca/cbc_news.py
@@ -1,23 +1,17 @@
 import datetime
-import json
 import re
 from typing import List, Optional
 
-import lxml
-import more_itertools
 from lxml.cssselect import CSSSelector
 from lxml.etree import XPath
-from lxml.html import document_fromstring
 
 from fundus.parser import ArticleBody, BaseParser, ParserProxy, attribute
-from fundus.parser.base_parser import Precomputed, logger
-from fundus.parser.data import LinkedDataMapping
+from fundus.parser.base_parser import function
 from fundus.parser.utility import (
     extract_article_body_with_selector,
+    extract_json_from_dom,
     generic_author_parsing,
     generic_date_parsing,
-    generic_topic_parsing,
-    get_meta_content,
 )
 
 
@@ -26,22 +20,14 @@ class CBCNewsParser(ParserProxy):
         _summary_selector = CSSSelector("h2.deck")
         _subheadline_selector = CSSSelector("div.story > h2")
         _paragraph_selector = CSSSelector("div.story > p")
-        _cbc_ld_selector: XPath = XPath("//script[@type='application/ld+json' or @id='initialStateDom']")
 
-        def _base_setup(self, html: str) -> None:
-            doc = lxml.html.document_fromstring(html)
-            ld_nodes = self._cbc_ld_selector(doc)
-            lds = []
-            for node in ld_nodes:
-                try:
-                    json_object = json.loads(re.sub(r"(window\.__INITIAL_STATE__ = |;$)", "", node.text_content()))
-                    if not json_object.get("@type"):
-                        json_object["@type"] = "FurtherDetails"
-                    lds.append(json_object)
-                except json.JSONDecodeError as error:
-                    logger.debug(f"Encountered {error!r} during LD parsing")
-            collapsed_lds = more_itertools.collapse(lds, base_type=dict)
-            self.precomputed = Precomputed(html, doc, get_meta_content(doc), LinkedDataMapping(collapsed_lds))
+        _cbc_ld_selector: XPath = XPath("//script[@id='initialStateDom']")
+
+        @function(priority=1)
+        def _parse_initial_state_dom(self):
+            state_dom_json = extract_json_from_dom(self.precomputed.doc, self._cbc_ld_selector)
+            for ld in state_dom_json:
+                self.precomputed.ld.add_ld(ld, "initialStateDom")
 
         @attribute
         def body(self) -> ArticleBody:
@@ -54,7 +40,7 @@ class CBCNewsParser(ParserProxy):
 
         @attribute
         def authors(self) -> List[str]:
-            return generic_author_parsing(self.precomputed.ld.bf_search("authorList"))
+            return generic_author_parsing(self.precomputed.ld.bf_search("author"))
 
         @attribute
         def publishing_date(self) -> Optional[datetime.datetime]:
@@ -62,14 +48,19 @@ class CBCNewsParser(ParserProxy):
 
         @attribute
         def title(self) -> Optional[str]:
-            if not (title := self.precomputed.meta.get("og:title")):
-                return title
-            return re.sub(r" \|.*", "", title)
+            return self.precomputed.ld.bf_search("headline")
 
         @attribute
         def topics(self) -> List[str]:
-            topic_dict = self.precomputed.ld.bf_search("keywords")
-            topic_list = [v.get("name") for v in topic_dict.get("tags")]
-            for v in topic_dict.get("concepts"):
-                topic_list.append(re.sub(r".*/", "", v.get("path")))
+            if (topic_dict := self.precomputed.ld.bf_search("keywords")) is None:
+                return []
+
+            # add locations
+            topic_list = [topic for location in topic_dict.get("tags") if (topic := location.get("name")) is not None]
+
+            # add subjects
+            for subject in topic_dict.get("concepts"):
+                if (path := subject.get("path")) is not None:
+                    topic_list.append(re.sub(r".*/", "", path))
+
             return topic_list
