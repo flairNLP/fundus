0da1ec371f49ee444c8144bd433834471f07f3e9
Adrian Breiding <ad123br@gmail.com>
2024-10-13 16:53:27 +0200

    add yomiuri shimbun

--- src/fundus/publishers/jp/__init__.py
+++ src/fundus/publishers/jp/__init__.py
@@ -1,5 +1,6 @@
 from fundus.publishers.base_objects import Publisher, PublisherGroup
-from fundus.publishers.jp.thejapannews import TheJapanNewsParser
+from fundus.publishers.jp.the_japan_news import TheJapanNewsParser
+from fundus.publishers.jp.yomiuri_shimbun import YomiuriShimbunParser
 from fundus.scraping.url import NewsMap, Sitemap
 
 
@@ -12,4 +13,11 @@ class JP(metaclass=PublisherGroup):
             Sitemap("https://japannews.yomiuri.co.jp/sitemap.xml"),
             NewsMap("https://japannews.yomiuri.co.jp/sitemap-news.xml"),
         ],
+    )
+
+    YomiuriShimbun = Publisher(
+        name="Yomiuri Shimbun",
+        domain="https://www.yomiuri.co.jp/",
+        parser=YomiuriShimbunParser,
+        sources=[Sitemap("https://www.yomiuri.co.jp/sitemap.xml")],
     )
