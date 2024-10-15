fedb7888703a53b4433260db641e55750720265a
Adrian Breiding <ad123br@gmail.com>
2024-10-15 18:04:06 +0200

    add el mundo

--- src/fundus/publishers/es/__init__.py
+++ src/fundus/publishers/es/__init__.py
@@ -1,4 +1,5 @@
 from fundus.publishers.base_objects import Publisher, PublisherGroup
+from fundus.publishers.es.el_mundo import ElMundoParser
 from fundus.publishers.es.el_pais import ElPaisParser
 from fundus.scraping.url import RSSFeed
 
@@ -10,5 +11,14 @@ class ES(metaclass=PublisherGroup):
         parser=ElPaisParser,
         sources=[
             RSSFeed("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada"),
+        ],
+    )
+    ElMundo = Publisher(
+        name="El Mundo",
+        domain="https://www.elmundo.es/",
+        parser=ElMundoParser,
+        sources=[
+            RSSFeed("https://e00-elmundo.uecdn.es/elmundo/rss/portada.xml"),
+            RSSFeed("https://e00-elmundo.uecdn.es/elmundo/rss/espana.xml"),
         ],
     )
---
+++ src/fundus/publishers/es/el_mundo.py
@@ -0,0 +1,44 @@
+import datetime
+from typing import List, Optional
+
+from lxml.etree import XPath
+
+from fundus.parser import ArticleBody, BaseParser, ParserProxy, attribute
+from fundus.parser.utility import (
+    extract_article_body_with_selector,
+    generic_author_parsing,
+    generic_date_parsing,
+    generic_topic_parsing,
+)
+
+
+class ElMundoParser(ParserProxy):
+    class V1(BaseParser):
+        _paragraph_selector = XPath("//div[@class='ue-c-article__body']/p")
+        _subheadline_selector = XPath("//div[@class='ue-c-article__body']/h2")
+        _summary_selector = XPath("//div[@class='ue-c-article__standfirst']//p")
+
+        @attribute
+        def body(self) -> ArticleBody:
+            return extract_article_body_with_selector(
+                self.precomputed.doc,
+                paragraph_selector=self._paragraph_selector,
+                subheadline_selector=self._subheadline_selector,
+                summary_selector=self._summary_selector,
+            )
+
+        @attribute
+        def publishing_date(self) -> Optional[datetime.datetime]:
+            return generic_date_parsing(self.precomputed.ld.bf_search("datePublished"))
+
+        @attribute
+        def title(self) -> Optional[str]:
+            return self.precomputed.ld.bf_search("headline")
+
+        @attribute
+        def authors(self) -> List[str]:
+            return generic_author_parsing(self.precomputed.ld.bf_search("author"))
+
+        @attribute
+        def topics(self) -> List[str]:
+            return generic_topic_parsing(self.precomputed.meta.get("keywords"))
