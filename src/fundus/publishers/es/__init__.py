972cd252928dd645e53bf9351abe0ddbdbd7702b
Adrian Breiding <ad123br@gmail.com>
2024-10-16 21:54:13 +0200

    optimize sitemaps, fix annotation

--- src/fundus/publishers/es/__init__.py
+++ src/fundus/publishers/es/__init__.py
@@ -1,5 +1,7 @@
 import datetime
 
+from dateutil.rrule import MONTHLY, rrule
+
 from fundus.publishers.base_objects import Publisher, PublisherGroup
 from fundus.publishers.es.el_pais import ElPaisParser
 from fundus.scraping.url import RSSFeed, Sitemap
@@ -12,13 +14,9 @@ class ES(metaclass=PublisherGroup):
         parser=ElPaisParser,
         sources=[RSSFeed("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada")]
         + [
-            Sitemap(f"https://elpais.com/sitemaps/{datetime.datetime.now().year}/{month:02d}/sitemap_0.xml")
-            for month in range(datetime.datetime.now().month, 0, -1)
-        ]
-        + [
-            Sitemap(f"https://elpais.com/sitemaps/{year}/{month:02d}/sitemap_0.xml")
-            for year in range(datetime.datetime.now().year - 1, 1976, -1)
-            for month in range(12, 0, -1)
-        ]
-        + [Sitemap(f"https://elpais.com/sitemaps/1976/{month:02d}/sitemap_0.xml") for month in range(12, 4, -1)],
+            Sitemap(f"https://elpais.com/sitemaps/{d.year}/{str(d.month).zfill(2)}/sitemap_0.xml")
+            for d in reversed(
+                list(rrule(MONTHLY, dtstart=datetime.datetime(1976, 5, 1), until=datetime.datetime.now()))
+            )
+        ],
     )
--- src/fundus/publishers/es/el_pais.py
+++ src/fundus/publishers/es/el_pais.py
@@ -19,7 +19,7 @@ class ElPaisParser(ParserProxy):
         _summary_selector = XPath("//div[@class='a_e_txt _df']//h2")
 
         @attribute
-        def body(self) -> ArticleBody:
+        def body(self) -> Optional[ArticleBody]:
             return extract_article_body_with_selector(
                 self.precomputed.doc,
                 paragraph_selector=self._paragraph_selector,
