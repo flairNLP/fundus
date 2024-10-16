0651a3de95596ad88a6b9454a9fc500552de2e26
Adrian Breiding <ad123br@gmail.com>
2024-10-16 21:44:44 +0200

    add sitemaps

--- src/fundus/publishers/es/__init__.py
+++ src/fundus/publishers/es/__init__.py
@@ -1,6 +1,8 @@
+import datetime
+
 from fundus.publishers.base_objects import Publisher, PublisherGroup
 from fundus.publishers.es.el_pais import ElPaisParser
-from fundus.scraping.url import RSSFeed
+from fundus.scraping.url import RSSFeed, Sitemap
 
 
 class ES(metaclass=PublisherGroup):
@@ -8,7 +10,15 @@ class ES(metaclass=PublisherGroup):
         name="El País",
         domain="https://elpais.com/",
         parser=ElPaisParser,
-        sources=[
-            RSSFeed("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada"),
-        ],
+        sources=[RSSFeed("https://feeds.elpais.com/mrss-s/pages/ep/site/elpais.com/portada")]
+        + [
+            Sitemap(f"https://elpais.com/sitemaps/{datetime.datetime.now().year}/{month:02d}/sitemap_0.xml")
+            for month in range(datetime.datetime.now().month, 0, -1)
+        ]
+        + [
+            Sitemap(f"https://elpais.com/sitemaps/{year}/{month:02d}/sitemap_0.xml")
+            for year in range(datetime.datetime.now().year - 1, 1976, -1)
+            for month in range(12, 0, -1)
+        ]
+        + [Sitemap(f"https://elpais.com/sitemaps/1976/{month:02d}/sitemap_0.xml") for month in range(12, 4, -1)],
     )
