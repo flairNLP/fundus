a0a0f7f2a95320b70c7ca2601703e423bf3660dc
Adrian Breiding <ad123br@gmail.com>
2024-10-16 22:41:06 +0200

    fix annotations

--- src/fundus/publishers/es/la_vanguardia.py
+++ src/fundus/publishers/es/la_vanguardia.py
@@ -19,7 +19,7 @@ class LaVanguardiaParser(ParserProxy):
         _summary_selector = XPath("//h2[@class='epigraph']")
 
         @attribute
-        def body(self) -> ArticleBody:
+        def body(self) -> Optional[ArticleBody]:
             return extract_article_body_with_selector(
                 self.precomputed.doc,
                 paragraph_selector=self._paragraph_selector,
