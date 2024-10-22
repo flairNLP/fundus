1ee5169719376ede1639e71f57a84e3788d3e331
Adrian Breiding <ad123br@gmail.com>
2024-10-22 18:53:21 +0200

    apply suggestions

--- src/fundus/publishers/es/la_vanguardia.py
+++ src/fundus/publishers/es/la_vanguardia.py
@@ -1,4 +1,5 @@
 import datetime
+import re
 from typing import List, Optional
 
 from lxml.etree import XPath
@@ -8,15 +9,24 @@ from fundus.parser.utility import (
     extract_article_body_with_selector,
     generic_author_parsing,
     generic_date_parsing,
+    generic_nodes_to_text,
     generic_topic_parsing,
 )
 
 
 class LaVanguardiaParser(ParserProxy):
     class V1(BaseParser):
-        _paragraph_selector = XPath("//div[@class='article-modules']//p[@class='paragraph']")
-        _subheadline_selector = XPath("//div[@class='article-modules']//h3[@class='subtitle']")
-        _summary_selector = XPath("//h2[@class='epigraph']")
+        _paragraph_selector = XPath(
+            "//div[@class='article-modules']//p[@class='paragraph']|"
+            "//div[@class='widget' and not(@id)]//p[not(@class='creditos')]"
+        )
+        _subheadline_selector = XPath(
+            "//div[@class='article-modules']//h3[@class='subtitle']|"
+            "//div[@class='widget' and not(@id)]//h2|//span[@class='ubicacion']"
+        )
+        _summary_selector = XPath("//h2[@class='epigraph']|//div[@id='slide-content-1']/p")
+
+        _topic_selector = XPath("//div[contains(@class, 'tags-container')]//li")
 
         @attribute
         def body(self) -> Optional[ArticleBody]:
@@ -37,8 +47,11 @@ class LaVanguardiaParser(ParserProxy):
 
         @attribute
         def authors(self) -> List[str]:
-            return generic_author_parsing(self.precomputed.ld.bf_search("author"))
+            return [
+                re.sub(r"(?u)\s*\u200b.*", "", author)
+                for author in generic_author_parsing(self.precomputed.ld.bf_search("author"))
+            ]
 
         @attribute
         def topics(self) -> List[str]:
-            return generic_topic_parsing(self.precomputed.meta.get("Keywords"))
+            return generic_topic_parsing(generic_nodes_to_text(self._topic_selector(self.precomputed.doc)))
