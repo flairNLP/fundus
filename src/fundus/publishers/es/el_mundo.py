2534f81d807d2b81ed61e08fc14345e4339c41df
Adrian Breiding <ad123br@gmail.com>
2024-12-02 12:39:26 +0100

    add images parsing

--- src/fundus/publishers/es/el_mundo.py
+++ src/fundus/publishers/es/el_mundo.py
@@ -3,12 +3,13 @@ from typing import List, Optional
 
 from lxml.etree import XPath
 
-from fundus.parser import ArticleBody, BaseParser, ParserProxy, attribute
+from fundus.parser import ArticleBody, BaseParser, Image, ParserProxy, attribute
 from fundus.parser.utility import (
     extract_article_body_with_selector,
     generic_author_parsing,
     generic_date_parsing,
     generic_topic_parsing,
+    image_extraction,
 )
 
 
@@ -41,4 +42,14 @@ class ElMundoParser(ParserProxy):
 
         @attribute
         def topics(self) -> List[str]:
-            return generic_topic_parsing(self.precomputed.meta.get("keywords"))
+            return [topic.split("/")[-1] for topic in generic_topic_parsing(self.precomputed.meta.get("keywords"))]
+
+        @attribute
+        def images(self) -> List[Image]:
+            return image_extraction(
+                doc=self.precomputed.doc,
+                paragraph_selector=self._paragraph_selector,
+                image_selector=XPath("//figure//img[contains(@class, 'article__image')]"),
+                caption_selector=XPath("./ancestor::figure//figcaption//span[contains(@class, 'description')]"),
+                author_selector=XPath("./ancestor::figure//figcaption//span[contains(@class, 'source')]/span"),
+            )
