c5c9c986c1442c80f677539552cf005e8d713353
Adrian Breiding <ad123br@gmail.com>
2024-12-21 20:20:37 +0100

    mypy

--- src/fundus/parser/utility.py
+++ src/fundus/parser/utility.py
@@ -542,12 +542,12 @@ def get_versions_from_node(
                 query_width = f"{param}:{value}"
 
     # get width, height and init calculator
-    if (width := source.get("width")) and width.replace(".", "", 1).isdigit():
-        width = float(width or 0) or None
+    if (src_width := source.get("width")) and src_width.replace(".", "", 1).isdigit():
+        width = float(src_width or 0) or None
     else:
         width = None
-    if (height := source.get("height")) and height.replace(".", "", 1).isdigit():
-        height = float(height or 0) or None
+    if (src_height := source.get("height")) and src_height.replace(".", "", 1).isdigit():
+        height = float(src_height or 0) or None
     else:
         height = None
     if width and height:
