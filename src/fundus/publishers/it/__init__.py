9b89050d660e9770a36ba8696c74432ca400d8b1
Max Dallabetta <max.dallabetta@googlemail.com>
2024-12-30 20:00:20 +0100

    black

--- src/fundus/publishers/it/__init__.py
+++ src/fundus/publishers/it/__init__.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
+
 from dateutil.rrule import MONTHLY, rrule
 
 from fundus.publishers.base_objects import Publisher, PublisherGroup
 from fundus.publishers.it.la_repubblica import LaRepubblicaParser
 from fundus.scraping.url import RSSFeed, Sitemap
+
 
 class IT(metaclass=PublisherGroup):
     LaRepubblica = Publisher(
@@ -12,14 +14,14 @@ class IT(metaclass=PublisherGroup):
         parser=LaRepubblicaParser,
         sources=[
             RSSFeed("https://www.repubblica.it/rss/homepage/rss2.0.xml"),
-        ] + [
+        ]
+        + [
             Sitemap(f"https://www.repubblica.it/sitemap-{date.strftime('%Y-%m')}.xml")
-            for date in reversed(list(rrule(
-                MONTHLY, 
-                dtstart=datetime(2020, 1, 1),
-                until=datetime.now()+timedelta(days=30)
-            )))
+            for date in reversed(
+                list(rrule(MONTHLY, dtstart=datetime(2020, 1, 1), until=datetime.now() + timedelta(days=30)))
+            )
         ],
     )
-    
+
+
 print(IT.LaRepubblica.source_mapping)
