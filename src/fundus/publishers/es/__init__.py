99593d03928440e71779a31520cec4bd6b03a2d2
Adrian Breiding <ad123br@gmail.com>
2025-01-13 19:52:07 +0100

    Add `LaVanguardia`

--- src/fundus/publishers/es/__init__.py
+++ src/fundus/publishers/es/__init__.py
@@ -6,6 +6,7 @@ from fundus.publishers.base_objects import Publisher, PublisherGroup
 from fundus.publishers.es.abc import ABCParser
 from fundus.publishers.es.el_mundo import ElMundoParser
 from fundus.publishers.es.el_pais import ElPaisParser
+from fundus.publishers.es.la_vanguardia import LaVanguardiaParser
 from fundus.scraping.url import NewsMap, RSSFeed, Sitemap
 
 
@@ -39,5 +40,22 @@ class ES(metaclass=PublisherGroup):
             NewsMap("https://www.abc.es/sitemap.xml"),
             RSSFeed("https://www.abc.es/rss/2.0/espana/"),
             RSSFeed("https://www.abc.es/rss/2.0/portada/"),
+        ],
+    )
+    LaVanguardia = Publisher(
+        name=" La Vanguardia",
+        domain="https://www.lavanguardia.com/",
+        parser=LaVanguardiaParser,
+        sources=[
+            NewsMap("https://www.lavanguardia.com/sitemap-google-news.xml"),
+            NewsMap("https://www.lavanguardia.com/sitemap-news-agencias.xml"),
+            RSSFeed("https://www.lavanguardia.com/rss/home.xml"),
+            RSSFeed("https://www.lavanguardia.com/rss/internacional.xml"),
+        ]
+        + [
+            Sitemap(f"https://www.lavanguardia.com/sitemap-noticias-{d.year}{str(d.month).zfill(2)}.xml.gz")
+            for d in reversed(
+                list(rrule(MONTHLY, dtstart=datetime.datetime(2019, 1, 1), until=datetime.datetime.now()))
+            )
         ],
     )
