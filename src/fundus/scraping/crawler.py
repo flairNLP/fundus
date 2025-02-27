ce89d9c0b5925b42066556576b82571f9e69560d
Max Dallabetta <max.dallabetta@googlemail.com>
2025-02-27 15:24:57 +0100

    properly release resources

--- src/fundus/scraping/crawler.py
+++ src/fundus/scraping/crawler.py
@@ -55,7 +55,7 @@ from fundus.publishers.base_objects import Publisher, PublisherGroup
 from fundus.scraping.article import Article
 from fundus.scraping.delay import Delay
 from fundus.scraping.filter import ExtractionFilter, Requires, RequiresAll, URLFilter
-from fundus.scraping.html import CCNewsSource
+from fundus.scraping.html import CCNewsSource, WebSource
 from fundus.scraping.scraper import CCNewsScraper, WebScraper
 from fundus.scraping.session import session_handler
 from fundus.scraping.url import URLSource
@@ -337,6 +337,10 @@ class CrawlerBase(ABC):
                     tuple(fitting_publishers), error_handling, build_extraction_filter(), url_filter
                 ):
                     if max_articles_per_publisher and article_count[article.publisher] == max_articles_per_publisher:
+                        if isinstance(self, Crawler) and not WebSource.__EVENTS__.is_event_set(
+                            "stop", article.publisher
+                        ):
+                            WebSource.__EVENTS__.set_event("stop", article.publisher)
                         if sum(article_count.values()) == len(self.publishers) * max_articles_per_publisher:
                             break
                         continue
@@ -426,6 +430,8 @@ class Crawler(CrawlerBase):
         self.ignore_robots = ignore_robots
         self.ignore_crawl_delay = ignore_crawl_delay
 
+        WebSource.__EVENTS__.clear_all()
+
     def _fetch_articles(
         self,
         publisher: Publisher,
@@ -464,15 +470,20 @@ class Crawler(CrawlerBase):
         article_iterators = [article_task(publisher) for publisher in publishers]
         yield from roundrobin(*article_iterators)
 
-    @staticmethod
     def _threaded_crawl(
-        publishers: Tuple[Publisher, ...], article_task: Callable[[Publisher], Iterator[Article]]
+        self, publishers: Tuple[Publisher, ...], article_task: Callable[[Publisher], Iterator[Article]]
     ) -> Iterator[Article]:
         result_queue: Queue[Union[Article, Exception]] = Queue(len(publishers))
         wrapped_article_task = queue_wrapper(result_queue, article_task)
 
-        with ThreadPool(processes=len(publishers) or None) as pool, session_handler.context(len(publishers), 1):
-            yield from pool_queue_iter(pool.map_async(wrapped_article_task, publishers), result_queue)
+        try:
+            with ThreadPool(processes=len(publishers) or None) as pool, session_handler.context(len(publishers), 1):
+                yield from pool_queue_iter(pool.map_async(wrapped_article_task, publishers), result_queue)
+        finally:
+            logger.debug(f"Shutting down {type(self).__name__!r} ...")
+            WebSource.__EVENTS__.set_all("stop")
+            pool.join()
+            logger.debug("Shutdown done")
 
     def _build_article_iterator(
         self,
@@ -598,24 +609,32 @@ class CCNewsCrawler(CrawlerBase):
         # As one could think, because we're downloading a bunch of files, this task is IO-bound, but it is actually
         # process-bound. The reason is that we stream the data and process it on the fly rather than downloading all
         # files and processing them afterward. Therefore, we utilize multiprocessing here instead of multithreading.
-        with Manager() as manager, Pool(
-            processes=min(self.processes, len(warc_paths)),
-            initializer=initializer,
-        ) as pool:
-            result_queue: Queue[Union[Article, Exception]] = manager.Queue(maxsize=1000)
+        try:
+            with Manager() as manager, Pool(
+                processes=min(self.processes, len(warc_paths)),
+                initializer=initializer,
+            ) as pool:
+                result_queue: Queue[Union[Article, Exception]] = manager.Queue(maxsize=1000)
 
-            # Because multiprocessing.Pool does not support iterators as targets,
-            # we wrap the article_task to write the articles to a queue instead of returning them directly.
-            wrapped_article_task: Callable[[str], None] = queue_wrapper(result_queue, article_task)
+                # Because multiprocessing.Pool does not support iterators as targets,
+                # we wrap the article_task to write the articles to a queue instead of returning them directly.
+                wrapped_article_task: Callable[[str], None] = queue_wrapper(result_queue, article_task)
 
-            # To avoid 503 errors we spread tasks to not start all at once
-            spread_article_task = random_sleep(wrapped_article_task, (0, 3))
+                # To avoid 503 errors we spread tasks to not start all at once
+                spread_article_task = random_sleep(wrapped_article_task, (0, 3))
 
-            # To avoid restricting the article_task to use only pickleable objects, we serialize it using dill.
-            serialized_article_task = dill_wrapper(spread_article_task)
+                # To avoid restricting the article_task to use only pickleable objects, we serialize it using dill.
+                serialized_article_task = dill_wrapper(spread_article_task)
 
-            # Finally, we build an iterator around the queue, exhausting the queue until the pool is finished.
-            yield from pool_queue_iter(pool.map_async(serialized_article_task, warc_paths), result_queue)
+                # Finally, we build an iterator around the queue, exhausting the queue until the pool is finished.
+                yield from pool_queue_iter(pool.map_async(serialized_article_task, warc_paths), result_queue)
+        finally:
+            logger.debug(f"Shutting down {type(self).__name__!r} ...")
+            logger.debug("Joining manager ...")
+            manager.join()
+            logger.debug("Joining pool ...")
+            pool.join()
+            logger.debug("Shutdown done")
 
     def _get_warc_paths(self) -> List[str]:
         # Date regex examples: https://regex101.com/r/yDX3G6/1
@@ -649,8 +668,11 @@ class CCNewsCrawler(CrawlerBase):
                 # use two threads per process, default two threads per core
                 max_number_of_threads = self.processes * 2
 
-                with ThreadPool(processes=min(len(urls), max_number_of_threads)) as pool:
-                    nested_warc_paths = pool.map(random_sleep(load_paths, (0, 3)), urls)
+                try:
+                    with ThreadPool(processes=min(len(urls), max_number_of_threads)) as pool:
+                        nested_warc_paths = pool.map(random_sleep(load_paths, (0, 3)), urls)
+                finally:
+                    pool.join()
 
         warc_paths: Iterator[str] = more_itertools.flatten(nested_warc_paths)
 
