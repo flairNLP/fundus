5733924324314bf903db97cbc65d232f9eee5c5b
Max Dallabetta <max.dallabetta@googlemail.com>
2025-04-17 18:27:14 +0200

    check if `default_language` is ISO 639 language code

--- tests/test_publisher_collection.py
+++ tests/test_publisher_collection.py
@@ -1,7 +1,23 @@
+from typing import List
+
+import lxml.html
 import pytest
+import requests
+from lxml.etree import XPath
 
 from fundus import PublisherCollection
 from fundus.publishers import PublisherGroup
+
+_language_code_selector = XPath("//table[contains(@class, 'wikitable') and @id='Table'] //td[@id] / @id")
+
+
+def get_two_letter_code() -> List[str]:
+    wiki_page = requests.get("https://en.wikipedia.org/wiki/List_of_ISO_639_language_codes")
+    two_letter_codes: List[str] = _language_code_selector(lxml.html.document_fromstring(wiki_page.content))
+    return two_letter_codes
+
+
+language_codes = get_two_letter_code()
 
 
 @pytest.mark.parametrize(
@@ -10,3 +26,9 @@ from fundus.publishers import PublisherGroup
 class TestPublisherCollection:
     def test_default_language(self, region: PublisherGroup):
         assert hasattr(region, "default_language"), f"Region {region.__name__!r} has no default language set"
+
+        default_language = getattr(region, "default_language")
+
+        assert (
+            default_language in language_codes
+        ), f"Default language {default_language!r} isn't a ISO 639 language code"
