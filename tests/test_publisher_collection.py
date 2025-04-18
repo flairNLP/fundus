f2e130ed05022f5b1d26c37fe9e15dd05af6fe22
Adrian Breiding <ad123br@gmail.com>
2025-04-18 13:23:52 +0200

    Add languages test

--- tests/test_publisher_collection.py
+++ tests/test_publisher_collection.py
@@ -1,12 +1,13 @@
 from typing import List
 
 import lxml.html
+import more_itertools
 import pytest
 import requests
 from lxml.etree import XPath
 
 from fundus import PublisherCollection
-from fundus.publishers import PublisherGroup
+from fundus.publishers import Publisher, PublisherGroup
 
 _language_code_selector = XPath("//table[contains(@class, 'wikitable') and @id='Table'] //td[@id] / @id")
 
@@ -20,10 +21,11 @@ def get_two_letter_code() -> List[str]:
 language_codes = get_two_letter_code()
 
 
-@pytest.mark.parametrize(
-    "region", [pytest.param(group, id=group.__name__) for group in PublisherCollection.get_subgroup_mapping().values()]
-)
 class TestPublisherCollection:
+    @pytest.mark.parametrize(
+        "region",
+        [pytest.param(group, id=group.__name__) for group in PublisherCollection.get_subgroup_mapping().values()],
+    )
     def test_default_language(self, region: PublisherGroup):
         assert hasattr(region, "default_language"), f"Region {region.__name__!r} has no default language set"
 
@@ -32,3 +34,10 @@ class TestPublisherCollection:
         assert (
             default_language in language_codes
         ), f"Default language {default_language!r} isn't a ISO 639 language code"
+
+    @pytest.mark.parametrize(
+        "publisher", [pytest.param(publisher, id=publisher.__name__) for publisher in PublisherCollection]
+    )
+    def test_source_languages(self, publisher: Publisher):
+        for source in more_itertools.flatten(publisher.source_mapping.values()):
+            assert source.languages.issubset(language_codes)
