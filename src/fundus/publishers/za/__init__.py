41256b82e56657e9e9f08c0aa5ac0e2118af002c
Adrian Breiding <ad123br@gmail.com>
2025-05-22 13:31:51 +0200

    Add `DailyMaverick`

--- src/fundus/publishers/za/__init__.py
+++ src/fundus/publishers/za/__init__.py
@@ -1,10 +1,25 @@
 from fundus.publishers.base_objects import Publisher, PublisherGroup
+from fundus.publishers.za.daily_maverick import DailyMaverickParser
 from fundus.publishers.za.times_live import TimesLiveParser
+from fundus.scraping.filter import inverse, regex_filter
 from fundus.scraping.url import NewsMap, Sitemap
 
 
 class ZA(metaclass=PublisherGroup):
     default_language = "en"
+
+    DailyMaverick = Publisher(
+        name="Daily Maverick",
+        domain="https://www.dailymaverick.co.za/",
+        parser=DailyMaverickParser,
+        sources=[
+            NewsMap("https://www.dailymaverick.co.za/news-sitemap.xml"),
+            Sitemap(
+                "https://www.dailymaverick.co.za/sitemap_index.xml",
+                sitemap_filter=inverse(regex_filter("article-sitemap")),
+            ),
+        ],
+    )
 
     TimesLive = Publisher(
         name="Times Live",
