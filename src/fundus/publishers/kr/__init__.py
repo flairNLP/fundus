c18c88f2adbbf3f7a1ca68357e86864e7ed3d8a6
zxxxv <jaewoongchoi46@gmail.com>
2025-05-31 15:02:47 +0000

    pass test

--- src/fundus/publishers/kr/__init__.py
+++ src/fundus/publishers/kr/__init__.py
@@ -1,7 +1,9 @@
 from fundus.publishers.base_objects import Publisher, PublisherGroup
-from fundus.scraping.url import RSSFeed, Sitemap
 from fundus.scraping.filter import inverse, regex_filter
+from fundus.scraping.url import RSSFeed, Sitemap
+
 from .mbn import MBNParser
+
 
 class KR(metaclass=PublisherGroup):
     default_language = "ko"
@@ -11,15 +13,14 @@ class KR(metaclass=PublisherGroup):
         domain="https://www.mk.co.kr/",
         parser=MBNParser,
         sources=[
-            #RSSFeed("https://www.mk.co.kr/rss/40300001/"),
+            # RSSFeed("https://www.mk.co.kr/rss/40300001/"),
             Sitemap(
                 "https://www.mk.co.kr/sitemap/latest-articles/",
-                sitemap_filter=inverse(regex_filter(r"/news/columnists/"))
+                sitemap_filter=inverse(regex_filter(r"/news/columnists/")),
             ),
             Sitemap(
                 "https://www.mk.co.kr/sitemap/daily-articles/",
-                sitemap_filter=inverse(regex_filter(r"/news/columnists/"))
+                sitemap_filter=inverse(regex_filter(r"/news/columnists/")),
             ),
         ],
     )
-
