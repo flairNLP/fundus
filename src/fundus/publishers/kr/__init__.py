7a744642daf1645268a64c85d76298966f26435f
Adrian Breiding <ad123br@gmail.com>
2025-06-04 21:43:57 +0200

    remove unused import

--- src/fundus/publishers/kr/__init__.py
+++ src/fundus/publishers/kr/__init__.py
@@ -1,6 +1,6 @@
 from fundus.publishers.base_objects import Publisher, PublisherGroup
 from fundus.scraping.filter import inverse, regex_filter
-from fundus.scraping.url import NewsMap, RSSFeed, Sitemap
+from fundus.scraping.url import NewsMap, Sitemap
 
 from .hankook_ilbo import HankookIlboParser
 from .mbn import MBNParser
