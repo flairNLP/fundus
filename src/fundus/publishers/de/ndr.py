7c245991bf7784e139a1f782d5b14b7fa76f7fc6
Adrian Breiding <ad123br@gmail.com>
2025-06-18 00:13:41 +0200

    Add `V1_1` for `NDR`

--- src/fundus/publishers/de/ndr.py
+++ src/fundus/publishers/de/ndr.py
@@ -1,6 +1,6 @@
 import datetime
 import re
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from lxml.cssselect import CSSSelector
 from lxml.etree import XPath
@@ -17,12 +17,14 @@ from fundus.parser.utility import (
 
 class NDRParser(ParserProxy):
     class V1(BaseParser):
+        VALID_UNTIL = datetime.date(2025, 6, 16)
+
         _paragraph_selector = XPath(
             "//div[@class='modulepadding copytext']/p[not(@class='textauthor' or @class='preface')] "
             "| //div[@class='modulepadding copytext']/ul/li"
         )
-        _summary_selector = CSSSelector(".preface")
-        _subheadline_selector = CSSSelector("article .modulepadding > h2")
+        _summary_selector: Union[XPath, CSSSelector] = CSSSelector(".preface")
+        _subheadline_selector: Union[XPath, CSSSelector] = CSSSelector("article .modulepadding > h2")
 
         @attribute
         def body(self) -> Optional[ArticleBody]:
@@ -60,5 +62,38 @@ class NDRParser(ParserProxy):
                 ),
                 relative_urls=XPath("string(//link[@rel='canonical']/@href)"),
                 caption_selector=XPath("./ancestor::div[contains(@class,'contentimage')]//span[@class='caption']"),
+                author_selector=re.compile(r"(?i)©\s*(ndr)?\s*(foto)?:?\s*(?P<credits>.+)"),
+            )
+
+    class V1_1(V1):
+        VALID_UNTIL = datetime.date.today()
+
+        _paragraph_selector = XPath("//article/p")
+        _subheadline_selector = XPath("//article/h2")
+        _summary_selector = XPath("//header/p[@class='preface']")
+
+        _bloat_keywords = ["hh", "regionalmeldungen", "News", "kurzmeldungen"]
+
+        @attribute
+        def topics(self) -> List[str]:
+            return [
+                topic
+                for topic in generic_topic_parsing(self.precomputed.meta.get("keywords"))
+                if topic not in self._bloat_keywords
+            ]
+
+        @attribute
+        def images(self) -> List[Image]:
+            return image_extraction(
+                doc=self.precomputed.doc,
+                paragraph_selector=self._paragraph_selector,
+                upper_boundary_selector=XPath("//article"),
+                relative_urls=XPath("string(//link[@rel='canonical']/@href)"),
+                image_selector=XPath(
+                    "//article//div[contains(@class,'contentimage') or contains(@class, 'herocontainer')]//picture//img"
+                ),
+                caption_selector=XPath(
+                    "./ancestor::div[contains(@class,'contentimage')]//span[contains(@class, 'caption')]"
+                ),
                 author_selector=re.compile(r"(?i)©\s*(ndr)?\s*(foto)?:?\s*(?P<credits>.+)"),
             )
