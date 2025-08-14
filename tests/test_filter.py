72f02f591f795c3679867eda03c9037455c618ba
Adrian Breiding <ad123br@gmail.com>
2025-08-14 19:03:15 +0200

    restore tests

--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,16 +1,10 @@
-from typing import Any, Dict
-
 from fundus import Requires
 from fundus.scraping.filter import RequiresAll
 
 
 class TestExtractionFilter:
     def test_requires(self):
-        extraction: Dict[str, Dict[str, Any]] = {
-            "a": {"value": "Some Stuff", "deprecated": False},
-            "b": {"value": [], "deprecated": False},
-            "c": {"value": True, "deprecated": False},
-        }
+        extraction = {"a": "Some Stuff", "b": [], "c": True}
 
         assert not Requires("a")(extraction)
 
@@ -20,11 +14,7 @@ class TestExtractionFilter:
 
         assert not Requires("c")(extraction)
 
-        extraction = {
-            "a": {"value": "Some Stuff", "deprecated": False},
-            "b": {"value": [], "deprecated": False},
-            "c": {"value": False, "deprecated": False},
-        }
+        extraction = {"a": "Some Stuff", "b": [], "c": False}
 
         assert (result := Requires("a", "b", "c")(extraction))
 
@@ -33,42 +23,19 @@ class TestExtractionFilter:
         assert not Requires("c", eval_booleans=False)(extraction)
 
     def test_requires_all(self):
-        extraction: Dict[str, Dict[str, Any]] = {
-            "a": {"value": "Some Stuff", "deprecated": False},
-            "b": {"value": [], "deprecated": False},
-            "c": {"value": False, "deprecated": False},
-        }
+        extraction = {"a": "Some Stuff", "b": [], "c": False}
 
         assert (result := RequiresAll()(extraction))
         assert result.missing_attributes == ("b",)
 
-        extraction = {
-            "a": {"value": "Some Stuff", "deprecated": False},
-            "c": {"value": False, "deprecated": False},
-        }
+        extraction = {"a": "Some Stuff", "c": False}
         assert not RequiresAll()(extraction)
 
         # test skip_boolean=False
-        extraction = {
-            "a": {"value": "Some Stuff", "deprecated": False},
-            "b": {"value": [], "deprecated": False},
-            "c": {"value": False, "deprecated": False},
-        }
+        extraction = {"a": "Some Stuff", "b": [], "c": False}
 
         assert (result := RequiresAll(eval_booleans=True)(extraction))
         assert sorted(result.missing_attributes) == sorted(("b", "c"))
 
-        extraction = {
-            "a": {"value": "Some Stuff", "deprecated": False},
-            "c": {"value": True, "deprecated": False},
-        }
+        extraction = {"a": "Some Stuff", "c": True}
         assert not RequiresAll(eval_booleans=True)(extraction)
-
-    def test_deprecation(self):
-        extraction: Dict[str, Dict[str, Any]] = {
-            "a": {"value": None, "deprecated": True},
-            "b": {"value": ["List", "is", "not", "empty"], "deprecated": False},
-            "c": {"value": False, "deprecated": False},
-        }
-        assert RequiresAll(force_deprecated=True)(extraction)
-        assert not RequiresAll(force_deprecated=False)(extraction)
