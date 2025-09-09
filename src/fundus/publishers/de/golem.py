5bb90c0250f981ffc7666ff64af97ffc55093c9c
Adrian Breiding <ad123br@gmail.com>
2025-09-09 17:50:08 +0200

    fix ˋGolemˋ

--- src/fundus/publishers/de/golem.py
+++ src/fundus/publishers/de/golem.py
@@ -1,6 +1,6 @@
 import datetime
 import re
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from lxml.cssselect import CSSSelector
 from lxml.etree import XPath
@@ -17,13 +17,15 @@ from fundus.parser.utility import (
 
 class GolemParser(ParserProxy):
     class V1(BaseParser):
+        VALID_UNTIL = datetime.date(2025, 8, 21)
+
         _bloat_regex = r"^Dieser Artikel enthält sogenannte Affiliate-Links"
-        _summary_selector = CSSSelector("hgroup > p")
+        _summary_selector: Union[XPath, CSSSelector] = CSSSelector("hgroup > p")
         _paragraph_selector = XPath(
             f"//section /p[not(@class='meta' or re:test(string(), '{_bloat_regex}'))]",
             namespaces={"re": "http://exslt.org/regular-expressions"},
         )
-        _subheadline_selector = CSSSelector("div > section > h2")
+        _subheadline_selector: Union[XPath, CSSSelector] = CSSSelector("div > section > h2")
 
         @attribute
         def body(self) -> Optional[ArticleBody]:
@@ -61,3 +63,10 @@ class GolemParser(ParserProxy):
                 upper_boundary_selector=XPath("//article"),
                 author_selector=re.compile(r"(?i)\(bild:(?P<credits>.*)\)"),
             )
+
+    class V1_1(V1):
+        VALID_UNTIL = datetime.date.today()
+
+        _paragraph_selector = XPath("//article//p[not(ancestor::div[@class='go-info-box__content'])]")
+        _subheadline_selector = XPath("//article/h2")
+        _summary_selector = XPath("//div[@class='go-article-header__intro']")
