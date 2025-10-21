958d8025597818b836a9753b507e659bd437fad8
nancyboukamel-ds <vera_nancy1@live.com>
2025-10-21 14:48:01 +0200

    lebanon publisher

---
+++ src/fundus/publishers/lb/__init__.py
@@ -0,0 +1,17 @@
+from fundus.publishers.base_objects import Publisher, PublisherGroup
+from fundus.publishers.lb.lbc_group import LBCGroupParser
+from fundus.scraping.url import NewsMap, RSSFeed, Sitemap
+
+class LB(metaclass=PublisherGroup):
+    default_language= "en"
+
+    LBCGroup=Publisher(
+        name = "LBC",
+        domain = "https://www.lbcgroup.tv",
+        parser = LBCGroupParser,
+        sources=[
+            RSSFeed("https://www.lbcgroup.tv/Rss/latest-news/en"),
+            NewsMap("https://www.lbcgroup.tv/newssitemap.xml"),
+            Sitemap("https://www.lbcgroup.tv/sitemap.xml"),
+        ],
+    )
---
+++ src/fundus/publishers/lb/lbc_group.py
@@ -0,0 +1,5 @@
+from fundus.parser import ParserProxy, BaseParser
+
+class LBCGroupParser(ParserProxy):
+    class V1(BaseParser):
+        pass
