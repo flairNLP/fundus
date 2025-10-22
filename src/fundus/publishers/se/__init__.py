0ac5fa2894427a9b43c97243430fa7918e3fcee7
marius <mariuswenk20@gmail.com>
2025-10-22 15:12:13 +0200

    Add publisher Aftonbladet

---
+++ src/fundus/publishers/se/__init__.py
@@ -0,0 +1,20 @@
+from fundus.publishers.base_objects import Publisher, PublisherGroup
+from fundus.scraping.filter import inverse, regex_filter
+from fundus.scraping.url import Sitemap
+
+from .aftonbladet import AftonbladetParser
+
+class SE(metaclass=PublisherGroup):
+    default_language = "se"
+    
+    Aftonbladet = Publisher(
+        name="Aftonbladet",
+        domain="https://www.aftonbladet.se/",
+        parser=AftonbladetParser,
+        sources=[
+            Sitemap("https://www.aftonbladet.se/sitemap.xml", 
+                    sitemap_filter=inverse(regex_filter("articles.xml")), 
+                    reverse=True,
+            ),
+        ],
+    )
---
+++ src/fundus/publishers/se/aftonbladet.py
@@ -0,0 +1,53 @@
+from datetime import datetime
+from typing import List, Optional
+
+from lxml.etree import XPath
+
+from fundus.parser import ArticleBody, BaseParser, Image, ParserProxy,attribute
+from fundus.parser.utility import (
+    extract_article_body_with_selector,
+    generic_author_parsing,
+    generic_date_parsing,
+    generic_topic_parsing,
+    image_extraction,
+)
+
+class AftonbladetParser(ParserProxy):
+    class V1(BaseParser):
+        _summary_selector = XPath(
+            "//p[contains(@data-test-tag,'lead-text')]"
+        )
+        _paragraph_selector = XPath(
+            "//p[starts-with(@class,'hyperion-css-') and not(contains(@data-test-tag,'lead-text'))]"
+        )
+
+        @attribute
+        def title(self) -> Optional[str]:
+            return self.precomputed.meta.get("og:title")
+
+        @attribute
+        def body(self) -> Optional[ArticleBody]:
+            return extract_article_body_with_selector(
+                self.precomputed.doc,
+                summary_selector=self._summary_selector,
+                paragraph_selector=self._paragraph_selector
+            )
+        
+        @attribute
+        def authors(self) -> List[str]:
+            return generic_author_parsing(self.precomputed.ld.bf_search("author"))
+
+        @attribute
+        def publishing_date(self) -> Optional[datetime]:
+            return generic_date_parsing(self.precomputed.meta["article:published_time"])
+        
+        @attribute
+        def topics(self) -> List[str]:
+            return generic_topic_parsing(self.precomputed.ld.bf_search("keywords"))
+
+        @attribute
+        def images(self) -> List[Image]:
+            return image_extraction(
+                doc=self.precomputed.doc,
+                paragraph_selector=self._paragraph_selector
+            )
