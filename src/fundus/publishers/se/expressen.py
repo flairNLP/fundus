47063eca002bfebced087b70d30e29e773e51bfc
Ghosts Shadow <35536281+ghostsshadow@users.noreply.github.com>
2025-10-23 23:46:18 +0200

    added rss, switched to json, added subheadlines testcase, refined image, added url filter, sitemap now in reverse

--- src/fundus/publishers/se/expressen.py
+++ src/fundus/publishers/se/expressen.py
@@ -19,6 +19,7 @@ class ExpressenParser(ParserProxy):
     class V1(BaseParser):
         _paragraph_selector = CSSSelector("div.article__body-text p")
         _summary_selector = CSSSelector("div.article__preamble")
+        _subheadline_selector = CSSSelector("div.article__body-text h2")
 
         @attribute
         def body(self) -> Optional[ArticleBody]:
@@ -26,11 +27,12 @@ class ExpressenParser(ParserProxy):
                 self.precomputed.doc,
                 paragraph_selector=self._paragraph_selector,
                 summary_selector=self._summary_selector,
+                subheadline_selector=self._subheadline_selector,
             )
 
         @attribute
         def title(self) -> Optional[str]:
-            return self.precomputed.meta.get("og:title")
+            return self.precomputed.ld.bf_search("headline")
 
         @attribute
         def publishing_date(self) -> Optional[datetime.datetime]:
@@ -46,9 +48,11 @@ class ExpressenParser(ParserProxy):
                 doc=self.precomputed.doc,
                 paragraph_selector=self._paragraph_selector,
                 image_selector=XPath("//figure//img"),
+                caption_selector=XPath("./ancestor::figure//figcaption//div[@class='rich-image__description']"),
+                author_selector=XPath("./ancestor::figure//figcaption//div[@class='rich-image__credit']"),
                 upper_boundary_selector=CSSSelector("div.article__body-text"),
             )
 
         @attribute
         def topics(self) -> List[str]:
-            return generic_topic_parsing(self.precomputed.ld.bf_search("articleSection"))
+            return [topic.split("/")[-1] for topic in generic_topic_parsing(self.precomputed.ld.bf_search("keywords"))]
