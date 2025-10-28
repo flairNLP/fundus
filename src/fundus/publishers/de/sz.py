4a15f5ae87290b2c79c6fda3558e026f98c3745c
Adrian Breiding <ad123br@gmail.com>
2025-10-28 23:30:26 +0100

    add LiveTicker support for SZ

--- src/fundus/publishers/de/sz.py
+++ src/fundus/publishers/de/sz.py
@@ -1,12 +1,14 @@
 import datetime
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from lxml.cssselect import CSSSelector
 from lxml.etree import XPath
 
 from fundus.parser import ArticleBody, BaseParser, Image, ParserProxy, attribute
+from fundus.parser.data import LiveTickerBody
 from fundus.parser.utility import (
     extract_article_body_with_selector,
+    extract_live_ticker_body_with_selector,
     generic_author_parsing,
     generic_date_parsing,
     generic_topic_parsing,
@@ -65,3 +67,36 @@ class SZParser(ParserProxy):
             "//div[@itemprop='articleBody']//h3[@data-manual='subheadline'] |"
             "//div[@itemprop='articleBody']//h2[@data-manual='subheadline']"
         )
+
+        _live_ticker_boundary_selector = XPath("//div[contains(@class, 'event__body')]")
+        _live_ticker_paragraph_selector = XPath(
+            "//article//div[contains(@class, 'event__body')]//li|//article//div[contains(@class, 'event__body')]//div[@class='tik4-rich-text tik4-rich-text--de']/div"
+        )
+        _live_ticker_subheadline_selector = XPath(
+            "//article//div[contains(@class, 'event__body')]//h2|//article//div[contains(@class, 'event__body')]//h3"
+        )
+        _live_ticker_date_selector = XPath("//article//div[contains(@class, 'event__body')]//time")
+        _live_ticker_author_selector = XPath(
+            "//article//div[contains(@class, 'event__body')]//div[@class='tik4-author__name']"
+        )
+        _live_ticker_summary_selector = XPath("//p[@data-manual='teaserText']")
+
+        @attribute
+        def body(self) -> Optional[Union[ArticleBody, LiveTickerBody]]:
+            if not self._live_ticker_boundary_selector(self.precomputed.doc):
+                return extract_article_body_with_selector(
+                    self.precomputed.doc,
+                    summary_selector=self._summary_selector,
+                    subheadline_selector=self._subheadline_selector,
+                    paragraph_selector=self._paragraph_selector,
+                )
+            else:
+                return extract_live_ticker_body_with_selector(
+                    self.precomputed.doc,
+                    summary_selector=self._live_ticker_summary_selector,
+                    subheadline_selector=self._live_ticker_subheadline_selector,
+                    paragraph_selector=self._live_ticker_paragraph_selector,
+                    entry_boundary_selector=self._live_ticker_boundary_selector,
+                    author_selector=self._live_ticker_author_selector,
+                    date_selector=self._live_ticker_date_selector,
+                )
