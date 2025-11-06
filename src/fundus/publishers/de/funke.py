c678006f98391fecdcb06073650cec78dd4b53d2
Adrian Breiding <ad123br@gmail.com>
2025-11-06 23:36:55 +0100

    trigger workflow

--- src/fundus/publishers/de/funke.py
+++ src/fundus/publishers/de/funke.py
@@ -106,4 +106,4 @@ class FunkeParser(ParserProxy):
     class V1_2(V1_1):
         VALID_UNTIL = datetime.date.today()
 
-        _topics_selector = XPath("//a[@data-track-v2-parentlabel='Mehr lesen über']")
+        _topics_selector = XPath("//a[@data-track-v2-parentlabel='Mehr lesen über'] ")
