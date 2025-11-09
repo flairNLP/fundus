f8111eb7c4ebdd2e83b9ad7f654ad9b29d220901
Adrian Breiding <ad123br@gmail.com>
2025-11-09 01:09:00 +0100

    Add some Independent Online newspapers

--- src/fundus/publishers/za/__init__.py
+++ src/fundus/publishers/za/__init__.py
@@ -1,5 +1,6 @@
 from fundus.publishers.base_objects import Publisher, PublisherGroup
 from fundus.publishers.za.daily_maverick import DailyMaverickParser
+from fundus.publishers.za.independent_online import IndependentOnlineParser
 from fundus.publishers.za.times_live import TimesLiveParser
 from fundus.scraping.filter import inverse, regex_filter
 from fundus.scraping.url import NewsMap, Sitemap
@@ -33,5 +34,36 @@ class ZA(metaclass=PublisherGroup):
             NewsMap("https://www.timeslive.co.za/sitemap/google-news/sunday-times/news/"),
             NewsMap("https://www.timeslive.co.za/sitemap/google-news/sunday-times/business/"),
             NewsMap("https://www.timeslive.co.za/sitemap/google-news/sunday-times-daily/news/"),
+        ],
+    )
+
+    DurbanLocal = Publisher(
+        name="Durban Local",
+        domain="https://www.durbanlocal.co.za/",
+        parser=IndependentOnlineParser,
+        sources=[
+            Sitemap("https://durbanlocal.co.za/sitemap/", sitemap_filter=inverse(regex_filter("/your-ethekwini/"))),
+        ],
+    )
+
+    Isolezwe = Publisher(
+        name="Isolezwe",
+        domain="https://www.isolezwe.co.za/",
+        parser=IndependentOnlineParser,
+        sources=[
+            Sitemap("https://isolezwe.co.za/sitemap/", sitemap_filter=inverse(regex_filter("/isolezwe/")), languages={"zu"}),
+        ],
+    )
+
+    IsolezweLesiXhosa = Publisher(
+        name="Isolezwe LesiXhosa",
+        domain="https://www.isolezwelesixhosa.co.za/",
+        parser=IndependentOnlineParser,
+        sources=[
+            Sitemap("https://isolezwelesixhosa.co.za/sitemap/isolezwe-lesixhosa/iindaba/", languages={"xh"}),
+            Sitemap("https://isolezwelesixhosa.co.za/sitemap/isolezwe-lesixhosa/ezemidlalo/", languages={"xh"}),
+            Sitemap("https://isolezwelesixhosa.co.za/sitemap/isolezwe-lesixhosa/ezoyolo/", languages={"xh"}),
+            Sitemap("https://isolezwelesixhosa.co.za/sitemap/isolezwe-lesixhosa/izimvo/", languages={"xh"}),
+            Sitemap("https://isolezwelesixhosa.co.za/sitemap/isolezwe-lesixhosa/entsimini/", languages={"xh"})
         ],
     )
