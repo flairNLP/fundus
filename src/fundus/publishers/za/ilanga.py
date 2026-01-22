b8eb52f5586baf80b83ebf898642fc8b322ceb45
Adrian Breiding <ad123br@gmail.com>
2026-01-22 18:11:23 +0100

    update selectors

--- src/fundus/publishers/za/ilanga.py
+++ src/fundus/publishers/za/ilanga.py
@@ -17,9 +17,22 @@ from fundus.parser.utility import (
 
 class IlangaParser(ParserProxy):
     class V1(BaseParser):
-        _paragraph_selector = XPath("//div[contains(@class,'post_content')]//p[text() and not(strong)]")
+        _paragraph_selector = XPath(
+            "(//div[contains(@class,'post_content')]//p["
+            "string() and "  # sometimes strong paragraphs are used for emphasis, hence filter by position
+            "not(position()<4 and strong and not(text())) and "  # Exclude author option 1
+            "not(position()<4 and string-length(text()) - string-length(translate(text(), ' ', '')) < 3) and"  # Exclude author option 2
+            "not(re:test(text(), '^\s*[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z.]{2,}\s*$'))"  # Exclude emails
+            "])[not(strong and not(text()) and preceding-sibling::*[position()=1 and self::figure])]",  # Exclude image captions
+            namespaces={"re": "http://exslt.org/regular-expressions"},
+        )
 
-        _author_selector = XPath("(//div[contains(@class,'post_content')]//p[position()=1])[strong and not(text())]")
+        _author_selector = XPath(
+            "(//div[contains(@class,'post_content')]//p["
+            "string() and position()<4])[(strong and not(text())) or "
+            "string-length(text()) - string-length(translate(text(), ' ', '')) < 3"
+            "]"
+        )
 
         @attribute
         def body(self) -> Optional[ArticleBody]:
@@ -48,6 +61,7 @@ class IlangaParser(ParserProxy):
                 doc=self.precomputed.doc,
                 paragraph_selector=self._paragraph_selector,
                 upper_boundary_selector=XPath("//div[contains(@class,'post_content')]"),
+                lower_boundary_selector=XPath("//div[@class='dtb-related-posts']"),
                 caption_selector=XPath(
                     "(./ancestor::figure/following-sibling::p[position()=1])[strong and not(text())]"
                 ),
