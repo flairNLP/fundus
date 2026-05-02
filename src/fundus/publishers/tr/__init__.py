1271e80d4a2e303ea54f46f985bf7ff849593e0c
Adrian Breiding <ad123br@gmail.com>
2026-05-02 10:16:03 +0200

    remove subdomain

--- src/fundus/publishers/tr/__init__.py
+++ src/fundus/publishers/tr/__init__.py
@@ -40,15 +40,5 @@ class TR(metaclass=PublisherGroup):
         parser=AnadoluAjansiParser,
         sources=[
             RSSFeed("https://www.aa.com.tr/tr/rss/default?cat=guncel"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/news?cat=gazze"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/news?cat=politika"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/news?cat=aktuel"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/news?cat=kultur-sanat"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/news?cat=bilim-teknoloji"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/news?cat=blog"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/news?cat=teyit-sozlugu"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/news?cat=ekonomi"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/news?cat=0"),
-            RSSFeed("https://www.aa.com.tr/tr/teyithatti/rss/video"),
         ],
     )
