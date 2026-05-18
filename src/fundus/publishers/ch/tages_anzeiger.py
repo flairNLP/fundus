ca3c8a928180a1d7b49f555b34599f4a051c5a4a
Adrian Breiding <ad123br@gmail.com>
2026-05-18 13:13:38 +0200

    Fix TagesAnzeiger

--- src/fundus/publishers/ch/tages_anzeiger.py
+++ src/fundus/publishers/ch/tages_anzeiger.py
@@ -13,11 +13,13 @@ from fundus.parser.utility import (
 )
 
 
-class TAParser(ParserProxy):
+class TagesAnzeigerParser(ParserProxy):
     class V1(BaseParser):
-        _summary_selector = CSSSelector("p.ContentHead_lead____SsS")
-        _subheadline_selector = CSSSelector("article > h2")
-        _paragraph_selector = CSSSelector(
+        VALID_UNTIL = datetime.date(2026, 5, 4)
+
+        _summary_selector: XPath = CSSSelector("p.ContentHead_lead____SsS")
+        _subheadline_selector: XPath = CSSSelector("article > h2")
+        _paragraph_selector: XPath = CSSSelector(
             "article > p"
             ":not(.ContentHead_lead____SsS)"
             ":not(.Feedback_root__fr_Mi)"
@@ -53,4 +55,21 @@ class TAParser(ParserProxy):
                 caption_selector=XPath("./ancestor::figure//figcaption/div[1]"),
                 author_selector=XPath("./ancestor::figure//figcaption/div[2]"),
                 lower_boundary_selector=CSSSelector("div.EndOfArticleSectionList_root__LJO7G"),
+            )
+
+    class V1_1(V1):
+        _paragraph_selector = XPath("//article//p[contains(@class, 'articleParagraph')]")
+        _summary_selector = XPath(
+            "//article//p[contains(@class, 'lead')] | "
+            "//article//div[contains(@class, 'SummaryList')]//*[self::strong or self::li]"
+        )
+        _subheadline_selector = XPath("//article//h2[contains(@class, 'crosshead')]")
+
+        @attribute
+        def images(self) -> List[Image]:
+            return image_extraction(
+                doc=self.precomputed.doc,
+                paragraph_selector=self._paragraph_selector,
+                caption_selector=XPath("./ancestor::figure//figcaption/div[1]"),
+                author_selector=XPath("./ancestor::figure//figcaption/div[2]"),
             )
