46b0c9248991847b0e97b53de0b7e79f4e343200
Adrian Breiding <ad123br@gmail.com>
2026-06-06 15:44:52 +0200

    Add parsers for CanadaCom, Financial Post, and The Province; refactor shared PostMedia logic

--- src/fundus/publishers/ca/__init__.py
+++ src/fundus/publishers/ca/__init__.py
@@ -1,7 +1,10 @@
 from fundus.publishers.base_objects import Publisher, PublisherGroup
+from fundus.publishers.ca.canada_com import CanadaComParser
 from fundus.publishers.ca.cbc_news import CBCNewsParser
+from fundus.publishers.ca.financial_post import FinancialPostParser
 from fundus.publishers.ca.globe_and_mail import TheGlobeAndMailParser
 from fundus.publishers.ca.national_post import NationalPostParser
+from fundus.publishers.ca.the_province import TheProvinceParser
 from fundus.scraping.url import NewsMap, RSSFeed, Sitemap
 
 # noinspection PyPep8Naming
@@ -9,6 +12,17 @@ from fundus.scraping.url import NewsMap, RSSFeed, Sitemap
 
 class CA(metaclass=PublisherGroup):
     default_language = "en"
+
+    CanadaCom = Publisher(
+        name="Canada.com",
+        domain="https://www.canada.com",
+        parser=CanadaComParser,
+        sources=[
+            NewsMap("https://o.canada.com/sitemap-news.xml"),
+            Sitemap("https://o.canada.com/sitemap-old.xml"),
+            RSSFeed("https://o.canada.com/feed"),
+        ],
+    )
 
     CBCNews = Publisher(
         name="CBC News",
@@ -21,6 +35,18 @@ class CA(metaclass=PublisherGroup):
         ],
         request_header={"User-Agent": "Fundus/2.0"},
     )
+
+    FinancialPost = Publisher(
+        name="Financial Post",
+        domain="https://financialpost.com",
+        parser=FinancialPostParser,
+        sources=[
+            NewsMap("https://financialpost.com/sitemap-news.xml"),
+            Sitemap("https://financialpost.com/sitemap-old.xml"),
+            RSSFeed("https://financialpost.com/feed"),
+        ],
+    )
+
     TheGlobeAndMail = Publisher(
         name="The Globe and Mail",
         domain="https://www.theglobeandmail.com",
@@ -28,6 +54,17 @@ class CA(metaclass=PublisherGroup):
         sources=[
             NewsMap("https://www.theglobeandmail.com/arc/outboundfeeds/news-sitemap-index/?outputType=xml"),
             NewsMap("https://www.theglobeandmail.com/arc/outboundfeeds/sitemap-index/?outputType=xml"),
+        ],
+    )
+
+    TheProvince = Publisher(
+        name="The Province",
+        domain="https://www.theprovince.com",
+        parser=TheProvinceParser,
+        sources=[
+            NewsMap("https://theprovince.com/sitemap-news.xml"),
+            Sitemap("https://theprovince.com/sitemap-old.xml"),
+            RSSFeed("https://theprovince.com/feed"),
         ],
     )
 
---
+++ src/fundus/publishers/ca/the_province.py
@@ -0,0 +1,26 @@
+from typing import List
+
+from lxml.etree import XPath
+
+from fundus.parser import Image, ParserProxy, attribute
+from fundus.parser.utility import image_extraction
+from fundus.publishers.shared.postmedia import PostMediaParser
+
+
+class TheProvinceParser(ParserProxy):
+    class V1(PostMediaParser.V1):
+        _paragraph_selector = XPath("//section[contains(@class, 'article-content')]//p[text() or span[text()]]")
+        _subheadline_selector = XPath(
+            "//section[contains(@class, 'article-content')]//*[(self::h4 or self::h3 or self::h2) and not(@class)]"
+        )
+
+        _bloat_topics = PostMediaParser.V1._bloat_topics | {"theprovince.com"}
+
+        @attribute
+        def images(self) -> List[Image]:
+            return image_extraction(
+                doc=self.precomputed.doc,
+                paragraph_selector=self._paragraph_selector,
+                caption_selector=XPath("./ancestor::figure/figcaption/span[@class='caption']"),
+                author_selector=XPath("./ancestor::figure/figcaption/span[@class='credit' or @class='distributor']"),
+            )
