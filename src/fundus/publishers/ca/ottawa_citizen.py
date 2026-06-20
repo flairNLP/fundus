0b80228a6743baa342f64f21f6e5de88114e9895
Adrian Breiding <ad123br@gmail.com>
2026-06-20 20:12:07 +0200

    apply reviewers suggestions

--- src/fundus/publishers/ca/ottawa_citizen.py
+++ src/fundus/publishers/ca/ottawa_citizen.py
@@ -6,10 +6,14 @@ from fundus.publishers.ca.national_post import NationalPostParser
 
 class OttawaCitizenParser(ParserProxy):
     class V1(NationalPostParser.V1_1):
-        _paragraph_selector = XPath("//div[@class='story-v2-content-element-inline']/p[span[text()] or text()]")
+        _paragraph_selector = XPath(
+            "//div[@class='story-v2-content-element-inline']/p[span[text()] or text()] | "
+            "//div[@class='story-v2-content-element-inline']/ul/li"
+        )
         _subheadline_selector = XPath(
-            "//div[@class='story-v2-content-element-inline']/h3 |"
-            "//div[@class='story-v2-content-element-inline']/p/*[self::strong or self::b]"
+            "//div[@class='story-v2-content-element-inline']/h3[not(re:test(string(), '(?i)^read the questions and answers'))] |"
+            "//div[@class='story-v2-content-element-inline']/p/*[(self::strong or self::b) and not(text())]",
+            namespaces={"re": "http://exslt.org/regular-expressions"},
         )
 
         _bloat_topics = NationalPostParser.V1_1._bloat_topics | {"ottawacitizen.com", "ottawa sun"}
