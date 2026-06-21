4e8640e62d4d20e53f6224c45844df505f1602a4
Adrian Breiding <ad123br@gmail.com>
2026-06-21 15:06:25 +0200

    fix selector

--- src/fundus/publishers/ca/ottawa_citizen.py
+++ src/fundus/publishers/ca/ottawa_citizen.py
@@ -8,10 +8,10 @@ class OttawaCitizenParser(ParserProxy):
     class V1(NationalPostParser.V1_1):
         _paragraph_selector = XPath(
             "//div[@class='story-v2-content-element-inline']/p[span[text()] or text()] | "
-            "//div[@class='story-v2-content-element-inline']/ul/li"
+            "//div[@class='story-v2-content-element-inline']/*[self::ol or self::ul]/li"
         )
         _subheadline_selector = XPath(
-            "//div[@class='story-v2-content-element-inline']/h3[not(re:test(string(), '(?i)^read the questions and answers'))] |"
+            "//div[@class='story-v2-content-element-inline']/*[(self::h2 or self::h3) and not(re:test(string(), '(?i)^read the questions and answers'))] |"
             "//div[@class='story-v2-content-element-inline']/p/*[(self::strong or self::b) and not(text())]",
             namespaces={"re": "http://exslt.org/regular-expressions"},
         )
