bf76ebd4d11054aa7cbaa432cc09d305b0dad9ad
Adrian Breiding <ad123br@gmail.com>
2026-07-11 18:01:28 +0200

    refactor(publisher): clean up deprecated V1 parser and comment out V2 draft
    Remove unused `VALID_UNTIL` for V1 parser and comment out the incomplete V2 parser draft.
    
    - Eliminate arbitrary `VALID_UNTIL` field for V1
    - Comment out V2 parser to avoid potential execution issues
    - Retain V2 implementation details for future development

--- src/fundus/publishers/fr/__init__.py
+++ src/fundus/publishers/fr/__init__.py
@@ -18,7 +18,7 @@ class FR(metaclass=PublisherGroup):
             Sitemap("https://www.lemonde.fr/sitemap_index.xml"),
             NewsMap("https://www.lemonde.fr/sitemap_news.xml"),
         ],
-        deprecated=True
+        deprecated=True,
     )
 
     EuronewsFR = Publisher(
