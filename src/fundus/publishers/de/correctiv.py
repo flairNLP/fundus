00c24a89fbcd0574db1eba7bc2d6a1fbadc9281b
Adrian Breiding <ad123br@gmail.com>
2026-08-29 09:48:51 +0200

    `fix(publisher): refine selectors, filters, and image parsing in Correctiv V2`

--- src/fundus/publishers/de/correctiv.py
+++ src/fundus/publishers/de/correctiv.py
@@ -67,17 +67,37 @@ class CorrectivParser(ParserProxy):
             )
 
     class V2(BaseParser):
+        _paragraph_bloat_sentence_beginnings = [
+            "Text and research",
+            "Editing",
+            "Fact-checking",
+            "Data work",
+            "Design",
+            "Redigatur",
+            "Redaktion",
+            "Mitarbeit",
+            "Faktencheck",
+            "Grafiken",
+        ]
+
         _summary_selector = XPath("//article//p[@class='detail__excerpt wp-block-paragraph']")
         _paragraph_selector = XPath(
-            r"//article//div[contains(@class, 'entry-content')]//p[not(re:test(string(.), '^(– |Redigatur|Redaktion|Mitarbeit|Faktencheck|Grafiken|\*\*\*)')) and (not(b or @class or @style) or @class='wp-block-paragraph')] |"
-            "//article//div[contains(@class, 'entry-content')]//li",
+            r"//article//div[contains(@class, 'entry-content') or contains(@class,'-container')]/p"
+            rf"[not(re:test(string(.), '^(– |{'|'.join(_paragraph_bloat_sentence_beginnings)}|\*\*\*)')) "
+            r"and ("
+            r"not((b and not(text())) or @class or @style) "
+            r"or @class='wp-block-paragraph' "
+            r"or contains(@class, 'text-base')"
+            r") and string-length(text())>1"
+            r"] |"
+            "//article//div[contains(@class, 'entry-content')]//*[self::li or self::blockquote]",
             namespaces={"re": "http://exslt.org/regular-expressions"},
         )
         _subheadline_selector = XPath(
-            "//article//div[contains(@class, 'entry-content')]//*[(self::h2 or self::h3) or (self::p and b)]"
+            "//article//div[contains(@class, 'entry-content')]/*[(self::h2 or self::h3) or (self::p and b and not(text()))]"
         )
 
-        _bloat_topics = {"Featured-auf-Startseite"}
+        _bloat_topics = {"Featured-auf-Startseite", "Related Articles am Ende"}
 
         @attribute
         def body(self) -> Optional[ArticleBody]:
@@ -90,7 +110,7 @@ class CorrectivParser(ParserProxy):
 
         @attribute
         def authors(self) -> List[str]:
-            return generic_author_parsing(self.precomputed.ld.xpath_search("//NewsArticle//author"))
+            return generic_author_parsing(self.precomputed.ld.xpath_search("//NewsArticle//author|//Article//author"))
 
         @attribute
         def publishing_date(self) -> Optional[datetime.datetime]:
@@ -109,5 +129,12 @@ class CorrectivParser(ParserProxy):
             return image_extraction(
                 doc=self.precomputed.doc,
                 paragraph_selector=self._paragraph_selector,
-                author_selector=re.compile(r"(?i)(foto|quelle):(?P<credits>.+)$"),
+                author_selector=[
+                    re.compile(
+                        r"(?i)(?<=\. )(((f|ph)otos?|(credit )?image|(symbol)?bild|collage|quelle):|©)?\s*(?P<credits>([^.:]|CORRECTIV\.|.com)+?)([.])?$"
+                    ),
+                    re.compile(r"\((.+:)?(?P<credits>[^):]+?)\)$"),
+                    re.compile(r"/(?P<credits>.+)$"),
+                ],
+                image_selector=XPath("//figure[@id]/img | //figure[@class='figure']/picture/img"),
             )
